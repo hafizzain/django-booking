b9132a9e40a3ab0c462a56cd4e9485f0ecd99718
HuzaifaYaqoob <zeevhyorg@gmail.com>
2022-07-20 17:11:33 +0500

    tenant custom middleware

--- Tenants/models.py
+++ Tenants/models.py
@@ -1,21 +1,35 @@
 from django.db import models
 
 from django_tenants.models import TenantMixin, DomainMixin
-
+from uuid import uuid4
 from django.contrib.auth.models import User
 # Create your models here.
 
 class Tenant(TenantMixin):
+    id = models.CharField(max_length=200, default=uuid4, primary_key=True, unique=True, editable=False)
     user  = models.ForeignKey(User, on_delete=models.CASCADE)
-    domain_name = models.CharField(max_length=20)
+    name = models.CharField(max_length=20)
+
+    is_active = models.BooleanField(default=True)
+    is_blocked = models.BooleanField(default=False)
+    is_deleted = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
 
 
     auto_create_schema = True
     auto_drop_schema = True
 
-
     def __str__(self):
-        return self.domain_name
+        return self.id
     
 class Domain(DomainMixin):
-    pass
+    id = models.CharField(max_length=200, default=uuid4, primary_key=True, unique=True, editable=False)
+
+    is_active = models.BooleanField(default=True)
+    is_deleted = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.id
+    
