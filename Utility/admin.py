84db59f13bce617384e4cf3c5b5df652c8569b91
HuzaifaYaqoob <zeevhyorg@gmail.com>
2022-08-01 18:08:52 +0500

    admin registration

--- Utility/admin.py
+++ Utility/admin.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
-from .models import Software
+from .models import Software, Country, State, City, Language
 
 @admin.register(Software)
 class SoftwareAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'is_active', 'is_deleted']
+
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'is_active', 'is_deleted']
+
+@admin.register(State)
+class StateAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'is_active', 'is_deleted']
+
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'is_active', 'is_deleted']
+
+@admin.register(Language)
+class LanguageAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'is_active', 'is_deleted']
