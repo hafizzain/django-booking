4c7d5ac2f9ca1927233751a23a9ac22cfeeb78cb
HuzaifaYaqoob <zeevhyorg@gmail.com>
2022-08-02 18:20:54 +0500

    twillio integration and other keys

--- Authentication/Constants/OTP.py
+++ Authentication/Constants/OTP.py
@@ -2,10 +2,32 @@
 
 import random, string
 from Authentication.models import VerificationOTP
+from twilio.rest import Client
 
+from django.conf import  settings
+
+def send_twillio_to_phone_number(user=None):
+    if user is not None:
+        try:
+            otp_obj = VerificationOTP.objects.get(user=user, code_for='Mobile')
+        except Exception as err:
+            print(err)
+            otp_obj = None
+
+        if otp_obj is not None:
+            print(otp_obj)
+            text_message = f'{otp_obj.code} is your One Time Password (OTP) for verification on NStyle. Do not share this password with anyone'
+            client = Client(settings.TWILLIO_ACCOUNT_SID, settings.TWILLIO_AUTH_TOKEN)
+            twilio_message = client.messages.create(
+                body=text_message,
+                from_=settings.TWILLIO_PHONE_NUMBER,
+                to=user.mobile_number
+            )
+            print('Sent Phone Number OTP')
+        else:
+            print('OTP not found')
 
 def generate_user_mobile_otp(user=None):
-
     if user is None:
         return
 
@@ -16,7 +38,4 @@ def generate_user_mobile_otp(user=None):
         code_for='Mobile'
     )
     otp.save()
-    send_new_user_otp_to_mobile_number(obj=otp)
-
-def send_new_user_otp_to_mobile_number(obj=None):
-    print('sending number.....')
+    send_twillio_to_phone_number(user=user)
