a598d7cb097e8c53bfa3da8126d7b916af4588c6
HuzaifaYaqoob <zeevhyorg@gmail.com>
2022-08-10 17:53:25 +0500

    allowed domain

--- NStyle/settings.py
+++ NStyle/settings.py
@@ -93,6 +93,8 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    'http://midtechdxb.com',
+    'https://midtechdxb.com',
 ]
 
 ROOT_URLCONF = 'NStyle.urls'
