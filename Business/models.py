696eb193f390e748965b5e36aa5b4851a3479765
HuzaifaYaqoob <zeevhyorg@gmail.com>
2022-08-15 11:51:56 +0500

    db design for booking settings

--- Business/models.py
+++ Business/models.py
@@ -228,3 +228,73 @@ class StockNotificationSetting(models.Model):
         return str(self.id)
 
 
+
+
+class BookingSetting(models.Model):
+    CANCEL_OR_RESCHEDULE_CHOICES = [
+        ('Anytime', 'Anytime'),
+        ('12_Hours_Prior_To_The_Appoinment', '12 Hours Prior To The Appoinment'),
+        ('24_Hours_Prior_To_The_Appoinment', '24 Hours Prior To The Appoinment'),
+        ('36_Hours_Prior_To_The_Appoinment', '36 Hours Prior To The Appoinment'),
+    ]
+
+    CLIENT_CAN_BOOK_CHOICES = [
+        ('Anytime', 'Anytime'),
+        ('5_Hours_Before', '5 Hours Before'),
+        ('12_Hours_Before', '12 Hours Before'),
+        ('24_Hours_Before', '24 Hours Before'),
+        ('36_Hours_Before', '36 Hours Before'),
+    ]
+
+    CONTROLS_TIME_SLOT_CHOICES = [
+        ('Anytime_In_The_Future', 'Anytime In The Future'),
+        ('No_More_Than_1_Months_In_The_Future', 'No More Than 1 Months In The Future'),
+        ('No_More_Than_2_Months_In_The_Future', 'No More Than 2 Months In The Future'),
+        ('No_More_Than_3_Months_In_The_Future', 'No More Than 3 Months In The Future'),
+    ]
+
+    TIME_SLOT_INTERVAL_CHOICES = [
+        ('15_Mins', '15 Mins'),
+        ('30_Mins', '30 Mins'),
+        ('45_Mins', '45 Mins'),
+        ('60_Mins', '60 Mins'),
+        ('120_Mins', '120 Mins'),
+    ]
+
+
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_booking_setting')
+    business = models.ForeignKey(Business, on_delete=models.SET_NULL, null=True, blank=True, related_name='business_booking_setting')
+
+    # ONLINE CANCELLATION AND RESCHEDULING 
+    cancel_or_reschedule = models.CharField(choices=CANCEL_OR_RESCHEDULE_CHOICES, default='Anytime', max_length=100)
+
+    # ONLINE BOOKING AVAILABILITY 
+    client_can_book = models.CharField(choices=CLIENT_CAN_BOOK_CHOICES, max_length=100, default='Anytime')
+    controls_time_slot = models.CharField(choices=CONTROLS_TIME_SLOT_CHOICES, max_length=100, default='Anytime_In_The_Future')
+    time_slots_interval = models.CharField(choices=TIME_SLOT_INTERVAL_CHOICES, default='15_Mins', max_length=50)
+    allow_client_to_select_team_member = models.BooleanField(default=True)
+
+    # ONLINE BOOKING ACTIVITY EMAILS 
+    send_to_client = models.BooleanField(default=False)
+    send_to_specific_email_address = models.BooleanField(default=False)
+
+    # BOOKING CONFIRMATION 
+    auto_confirmation = models.BooleanField(default=False)
+    admin_confirmation = models.BooleanField(default=False)
+
+    # BOOKING FORM
+    start_time = models.BooleanField(default=True)
+    services = models.BooleanField(default=True)
+    duration = models.BooleanField(default=False)
+    choose_team_member = models.BooleanField(default=True)
+    select_payment_type = models.BooleanField(default=False)
+    initial_deposit = models.BooleanField(default=False)
+
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=now)
+
+
+    def __str__(self):
+        return str(self.id)
