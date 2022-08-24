2aea5430a9837540b42cca31bfbcabb784ed82e0
Abdullah-Ajmal147 <abajmal211@gmail.com>
2022-08-24 17:01:15 +0500

    Create Employe

--- Employee/urls/v1/urls.py
+++ Employee/urls/v1/urls.py
@@ -3,5 +3,9 @@ from django.urls import path, include
 from Employee.views.v1 import views
  
 urlpatterns = [
-     path('get_employee/', views.getEmployees),
+    
+    #Employee Data
+     path('get_employees/', views.get_Employees),
+     path('create_employe/', views.create_Employ)
+     
  ]
