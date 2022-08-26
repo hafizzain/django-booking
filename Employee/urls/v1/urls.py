683ae040e20c1a85cb02a73436cfff2060d05ff4
HuzaifaYaqoob <zeevhyorg@gmail.com>
2022-08-26 09:56:51 +0500

    url group staff added

--- Employee/urls/v1/urls.py
+++ Employee/urls/v1/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('get_employees/', views.get_Employees),
     path('create_employee/', views.create_employee),
     path('delete_employee/', views.delete_employee),
+
+    # Staff GROUP URLS 
+    path('get_staff_group/', views.delete_employee),
+
 ]
