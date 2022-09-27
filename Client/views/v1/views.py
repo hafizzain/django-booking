551ad4f47110493ccb5a579a4f8ba24f83d208f8
HuzaifaYaqoob <zeevhyorg@gmail.com>
2022-09-27 15:28:10 +0500

    tkinter and client wrong import

--- Client/views/v1/views.py
+++ Client/views/v1/views.py
@@ -1,6 +1,3 @@
-from http import client
-from re import M
-from tkinter import N
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
