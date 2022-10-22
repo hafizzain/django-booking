d4ae93be68a36643267509c32845fe58d849af23
Abdullah-Ajmal147 <abajmal211@gmail.com>
2022-10-22 16:04:44 +0500

    Employee added in service or customization

--- Sale/views/v1/views.py
+++ Sale/views/v1/views.py
@@ -174,6 +174,7 @@ def create_service(request):
                    service = service_obj,
                    employee = employe
                    )
+                service_obj.employee.add(employe)
             except:
                 employees_error.append(str(err))
                 pass
