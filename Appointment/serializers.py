25fefbd044387e184009a81b58b9bf7088c452d0
Abdullah-Ajmal147 <abajmal211@gmail.com>
2022-10-25 14:50:13 +0500

    change in cencel or resdule or get_or_create in emloyee

--- Appointment/serializers.py
+++ Appointment/serializers.py
@@ -396,7 +396,7 @@ class SingleAppointmentSerializer(serializers.ModelSerializer):
         model = AppointmentService
         fields= ('id', 'location','client','service',
                  'appointment_time', 'end_time', 'member',
-                 'appointment_status', 'currency', 'booked_by', 'booking_id', 'appointment_date', 'client_type', 'duration' , 'notes'
+                 'appointment_status', 'currency', 'booked_by', 'booking_id', 'appointment_date', 'client_type', 'duration' , 'notes', 'is_favourite'
             )
         
 
