a8a9e6f1f16fcd3286429727b9a87e3749538185
Abdullah-Ajmal147 <abajmal211@gmail.com>
2023-01-05 17:56:07 +0500

    create new api get product reports and migrations new table of reports

--- Business/views/v1/views.py
+++ Business/views/v1/views.py
@@ -2911,7 +2911,7 @@ def get_check_availability(request):
                 dt = datetime.strptime(start_time, "%H:%M:%S")
                 start_time = dt.time()
                 try:
-                    employee = Employee.objects.get(id = str(emp_id))
+                    employee = Employee.objects.get(id = emp_id)
                     av_staff_ids = AppointmentService.objects.filter(
                     #member__id__in = empl_list,
                     #business = ,
