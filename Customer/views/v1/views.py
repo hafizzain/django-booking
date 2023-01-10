d60c7ccc54e0c832cde428a76978cd8fe8ce9e36
Abdullah-Ajmal147 <abajmal211@gmail.com>
2023-01-10 12:46:58 +0500

    max data memory size

--- Customer/views/v1/views.py
+++ Customer/views/v1/views.py
@@ -506,7 +506,7 @@ def get_client_appointment(request):
                 'status_code' : 400,
                 'status_code_text' : 'Invalid Data',
                 'response' : {
-                    'message' : 'Invalid Tenat Id',
+                    'message' : 'Invalid Tenant Id',
                     'error_message' : str(err),
                 }
             },
