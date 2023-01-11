38c55b0a4da6df4519430e9b1fa0d6f927b0cbc5
Abdullah-Ajmal147 <abajmal211@gmail.com>
2023-01-11 16:22:45 +0500

    update api update direct flat

--- Promotions/views/v1/views.py
+++ Promotions/views/v1/views.py
@@ -392,6 +392,7 @@ def update_directorflat(request):
                     ExceptionRecord.objects.create(text = f'prmotion test {test} type of {type(test)} equal to{test == True} or {test}')
                     if test:
                         dayrestriction.delete()
+                        dayrestriction.save()
                         pass
                     dayrestriction.day = day
                     dayrestriction.save()
