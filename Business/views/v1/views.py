db0d9fbb65c964c565ac4a684a476066fe74e3b6
Abdullah-Ajmal147 <abajmal211@gmail.com>
2023-01-18 17:39:39 +0500

    promotion migrations fixed price and create apis

--- Business/views/v1/views.py
+++ Business/views/v1/views.py
@@ -3127,7 +3127,7 @@ def get_employee_appointment(request):
         all_emp = Employee.objects.filter(is_deleted=False, 
                         location__id = business.id, 
                         employee_employedailyschedule__is_vacation = False,
-                        employee_selected_service__service__id = service_id,
+                        employee_selected_service__service__id = service,
                         ).order_by('-created_at')
         for emp in all_emp:
         
