b48dfb6058739284b2062bbaef80356c6685af34
Abdullah-Ajmal147 <abajmal211@gmail.com>
2023-01-19 18:17:34 +0500

    promotion free service migrations and apis customization data

--- Business/views/v1/views.py
+++ Business/views/v1/views.py
@@ -2988,21 +2988,27 @@ def get_check_availability(request):
                         is_vacation = False,
                         date = date,
                         )      
-                    if daily_schedule.start_time >= start_time and daily_schedule.end_time <= start_time :
-                        return Response(
-                        {
-                            'status' : True,
-                            'status_code' : 200,
-                            'response' : {
-                                'message' : 'Employee Day Off',
-                                'error_message' : f'This Employee day off, {employee.full_name} date {date} pass data',
-                                'Availability': False
-                            }
-                        },
-                        status=status.HTTP_200_OK
-                    )
+                    if daily_schedule.start_time >= start_time:
+                        if daily_schedule.end_time_shift != None:
+                           if daily_schedule.end_time_shift <= start_time:
+                               pass
+                        elif daily_schedule.end_time <= start_time:
+                            pass
+                            
+                        else:
+                            return Response(
+                            {
+                                'status' : True,
+                                'status_code' : 200,
+                                'response' : {
+                                    'message' : 'Employee Day Off',
+                                    'error_message' : f'This Employee day off, {employee.full_name} date {date} pass data',
+                                    'Availability': False
+                                }
+                            },
+                            status=status.HTTP_200_OK
+                        )
                         
-                        pass
                     # elif daily_schedule.start_time_shift > start_time and start_time < daily_schedule.end_time_shift:
                     #     pass
                     else:
