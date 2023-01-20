fbe3cc9301f1be36c5b6a7122c8174ff9b839fc1
Abdullah-Ajmal147 <abajmal211@gmail.com>
2023-01-20 14:58:28 +0500

    create api bundle fixed price and end points

--- Appointment/serializers.py
+++ Appointment/serializers.py
@@ -241,9 +241,11 @@ class EmployeeAppointmentSerializer(serializers.ModelSerializer):
                 new_end_time = None
 
                 for dt in selected_data:
-                    # if ((dt['range_start'] >= appointment_time and dt['range_end'] <= end_time) and dt['date'] == app_date):
-                    #     print('tested is sucess')
-                        #find_values.append(dt)
+                    if ((dt['range_start'] >= appointment_time and dt['range_end'] <= end_time) and dt['date'] == app_date):
+                        ExceptionRecord.objects.create(
+                            text = f'tested successfully'
+                        )
+                        find_values.append(dt)
                     # else:
                     #     new_start_time = appointment_time
                     
@@ -255,22 +257,23 @@ class EmployeeAppointmentSerializer(serializers.ModelSerializer):
                     # if str(dt['range_end']) == str(appointment_time) and dt['date'] == app_date:
                     #     find_values.append(dt)
                     #     new_end_time = end_time
+                    
                        ##OLD
                         
-                    if ((dt['range_start'] == appointment_time or str(dt['range_start']) == str(end_time)) and dt['date'] == app_date):
-                        find_values.append(dt)
-                    else:
-                        new_start_time = appointment_time
+                    # if ((dt['range_start'] == appointment_time or str(dt['range_start']) == str(end_time)) and dt['date'] == app_date):
+                    #     find_values.append(dt)
+                    # else:
+                    #     new_start_time = appointment_time
                     
-                    if str(dt['range_end']) == str(end_time) and dt['date'] == app_date:
-                        find_values.append(dt)
-                    else:
-                        # new_end_time = end_time
-                        pass
+                    # if str(dt['range_end']) == str(end_time) and dt['date'] == app_date:
+                    #     find_values.append(dt)
+                    # else:
+                    #     # new_end_time = end_time
+                    #     pass
 
-                    if str(dt['range_end']) == str(appointment_time) and dt['date'] == app_date:
-                        find_values.append(dt)
-                        new_end_time = end_time
+                    # if str(dt['range_end']) == str(appointment_time) and dt['date'] == app_date:
+                    #     find_values.append(dt)
+                    #     new_end_time = end_time
 
 
                 if len(find_values) > 0:
