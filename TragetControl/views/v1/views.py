b4a8ed57e79e34e7985c934707fa364160e7623d
Abdullah-Ajmal147 <abajmal211@gmail.com>
2023-01-27 17:23:16 +0500

    Prmotion- packges discont migrations and other things done

--- TragetControl/views/v1/views.py
+++ TragetControl/views/v1/views.py
@@ -468,7 +468,7 @@ def create_storetarget(request):
         tier_store.year = c_year
         tier_store.save()
         
-        if is_primary == 'true':
+        if is_primary == 'True':
             tier_store.is_primary = True
         else:
             tier_store.is_primary = False
