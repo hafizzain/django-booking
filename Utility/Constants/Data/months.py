7b2427466be207d9d8c8e6bce08f9a7ba334f92a
Abdullah-Ajmal147 <abajmal211@gmail.com>
2023-01-27 16:40:43 +0500

    Prmotion- packges discont migrations and other things done

--- Utility/Constants/Data/months.py
+++ Utility/Constants/Data/months.py
@@ -6,16 +6,16 @@ MONTHS = {0: {'value': 'January'}, 1: {'value': 'February'}, 2: {'value': 'March
     'value': 'July'}, 7: {'value': 'August'}, 8: {'value': 'September'}, 9: {'value': 'October'}, 10: {'value': 'November'}, 11: {'value': 'December'}}
 
 MONTH_DICT = {
-        (1, 'January'),
-        (2, 'February'),
-        (3, 'March'),
-        (4, 'April'),
-        (5, 'May'),
-        (6, 'June'),
-        (7, 'July'),
-        (8, 'August'),
-        (9, 'September'),
-        (10, 'October'),
-        (11, 'November'),
-        (12, 'December'),
+        1 : 'January',
+        2 : 'February',
+        3 : 'March',
+        4 : 'April',
+        5 : 'May',
+        6 : 'June',
+        7 : 'July',
+        8 : 'August',
+        9 : 'September',
+        1 : 'October',
+        1 : 'November',
+        1 : 'December',
 }
