7748f0dbf1aaa062de024bc86bc0cebaa83f48be
Abdullah-Ajmal147 <abajmal211@gmail.com>
2023-01-31 12:19:09 +0500

    migration in acccount type and customer and views and url

--- Employee/views/v1/views.py
+++ Employee/views/v1/views.py
@@ -3648,7 +3648,7 @@ def employee_login(request):
     for da in employee_tenant:
         data.append(da)
     try:
-        employee_tenant = EmployeeTenantDetail.objects.get(user = user)
+        employee_tenant = EmployeeTenantDetail.objects.get(user = str(user))
     except Exception as err:
         return Response(
             {
