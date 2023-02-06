7833f5008a799b2807d0c562e33fc0d0afa0ceb6
Abdullah-Ajmal147 <abajmal211@gmail.com>
2023-02-06 14:50:37 +0500

    added desigination in api and some changing in other apis

--- Appointment/views/v1/views.py
+++ Appointment/views/v1/views.py
@@ -178,7 +178,7 @@ def get_appointments_device(request):
 
     
     try:
-        appointment = Appointment.objects.filter(appointment_services__member = employee )       
+        appointment = Appointment.objects.filter(appointment_services__member = employee ).distinct()    
 #appointment = Appointment.objects.filter(member = employee )
         # .prefetch_related(
         #     Prefetch('appointment_services', queryset=AppointmentService.objects.filter(member=employee))#[0]
