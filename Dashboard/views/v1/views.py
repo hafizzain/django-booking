1f85eb0ec169bf1f33718d55cbc38bef90b24e91
dawood <dawoodmunir1@gmail.com>
2023-02-16 15:51:15 +0500

    API changing with acheived and total setters

--- Dashboard/views/v1/views.py
+++ Dashboard/views/v1/views.py
@@ -352,12 +352,15 @@ def get_dashboard_target_overview(request):
     # # retail_target = StaffTarget.objects.filter(user = employee_id)
     membership_target = MemberShipOrder.objects.filter(member = employee)
 
+    
     s=0
     r=0
     v = voucher_target.count()
     m = membership_target.count()
     total_set = 0
     achieved_target= len(achieved_target_member)
+
+    
 
     print(targets)
 
@@ -375,8 +378,24 @@ def get_dashboard_target_overview(request):
     print(r)
     print(v)
     print(m)
-
-    
+    v1=0
+    v2=0
+    m1=0
+    m2=0
+    s1=0
+    s2=0
+    r1=0
+    r2=0
+    for target in targets :
+        v1 = target.voucher_target.acheived_target.count()
+        v2 = target.voucher_target.service_target.count()
+        m1 = target.membership_target.acheived_target.count()
+        m2 = target.membership_target.service_target.count()
+        s1 = target.service_target.acheived_target.count()
+        s2 = target.service_target.service_target.count()
+        r1 = target.retail_target.acheived_target.count()
+        r2 = target.retail_target.service_target.count()
+        
     return Response(
             {
                 'status' : 200,
@@ -387,10 +406,14 @@ def get_dashboard_target_overview(request):
                     'employee_id' : employee_id,
                     'total_set' : total_set,
                     'achieved_target' : achieved_target,
-                    'service_target' : s,
-                    'retail_target' : r,
-                    'voucher_target' : v,
-                    'membership_target' : m,
+                    'service_target_set' : s1,
+                    'service_target_acheived' : s2,
+                    'retail_target_set' : r1,
+                    'retail_target_acheived' : r2,
+                    'voucher_target_set' : v1,
+                    'voucher_target_acheived' : v2,
+                    'membership_target_set' : m1,
+                    'membership_target_acheived' : m2,
                 }
             },
             status=status.HTTP_200_OK
