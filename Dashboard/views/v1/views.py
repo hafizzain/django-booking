992efd134189ccb07ab76d217b7eefe11733efcb
Abdullah-Ajmal147 <abajmal211@gmail.com>
2023-02-21 12:05:49 +0500

    sale new api and some change in exiting api

--- Dashboard/views/v1/views.py
+++ Dashboard/views/v1/views.py
@@ -1,11 +1,9 @@
 from django.conf import settings
 from operator import ge
-from Appointment.serializers import CheckoutSerializer
-
 
 from Order.models import Order, ProductOrder,VoucherOrder,MemberShipOrder,ServiceOrder,Checkout
 from Order.models import Checkout, ProductOrder,VoucherOrder,MemberShipOrder,ServiceOrder
-from Sale.serializers import AppointmentCheckoutSerializer
+from Sale.serializers import AppointmentCheckoutSerializer, CheckoutSerializer, OrderSerializer
 # from TragetControl.models import TierStoreTarget
 
 from Utility.Constants.Data.months import  FIXED_MONTHS, MONTH_DICT, MONTHS, MONTHS_DEVICE
@@ -821,7 +819,7 @@ def get_total_sales_device(request):
     checkout_order = Order.objects.filter(is_deleted=False, member__id=employee_id).order_by('-created_at')
     for i in checkout_order:
         total_sale  += int(i.total_price)
-    serialized = CheckoutSerializer(checkout_order, many=True, context={'request': request})
+    serialized = OrderSerializer(checkout_order, many=True, context={'request': request})
     data.extend(serialized.data)
 
     appointment_checkout = AppointmentCheckout.objects.filter(appointment_service__appointment_status='Done', member__id=employee_id)
