06683330f54c63cf13d4480b06869f17dd061b52
Abdullah-Ajmal147 <abajmal211@gmail.com>
2023-03-03 15:34:29 +0500

    employee email migrations and discount migrations in order tabel

--- Sale/serializers.py
+++ Sale/serializers.py
@@ -810,7 +810,7 @@ class AppointmentCheckout_ReportsSerializer(serializers.ModelSerializer):
         if appointment_checkout:
             tip = appointment_checkout.tip
             payment_type = appointment_checkout.payment_method
-            if obj.appointment.client.full_name is not None:
+            if obj.appointment.client is not None:
                 client =  obj.appointment.client.full_name,
             else:
                 client = ''
