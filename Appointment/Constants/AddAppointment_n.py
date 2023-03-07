8d6e7c9cccdf12866fc0c18795079390a1ca6cfa
dawood <dawoodmunir1@gmail.com>
2023-03-07 15:59:41 +0500

    create emails for appointments

---
+++ Appointment/Constants/AddAppointment_n.py
@@ -0,0 +1,84 @@
+from urllib import response
+from django.template.loader import render_to_string
+from django.core.mail import EmailMultiAlternatives
+from django.utils.html import strip_tags
+from Appointment.models import Appointment, AppointmentService
+from Business.models import ClientNotificationSetting, StaffNotificationSetting
+
+from django_tenants.utils import tenant_context
+from django.conf import settings
+from datetime import datetime,date
+
+from Utility.models import ExceptionRecord
+
+
+def Add_appointment(appointment = None, tenant = None):
+    if appointment is None or tenant is None:
+        ExceptionRecord.objects.create(
+            text='Appointment, Tenant Is None'
+        )
+
+    with tenant_context(tenant):
+        try:
+            appointment =  AppointmentService.objects.filter(appointment = appointment)                
+            for appo in appointment:
+                
+                email_c =appo.appointment.client.email
+                name_c =appo.appointment.client.full_name
+                ser_name = appo.service.name
+                dat = appo.appointment_date
+                mem_email = appo.member.email
+                mem_name = appo.member.full_name
+                mem_id= appo.member.employee_id
+                client_type= appo.appointment.client_type
+                name = appo.appointment.client.full_name
+
+                staff = appointment.member.full_name
+                loc_name = appointment.business_address.address_name
+                dur = appointment.duration
+                phon = appointment.member.mobile_number
+                
+                try:
+                    staff_email = StaffNotificationSetting.objects.get(business = str(appo.appointment.business))
+                    client_email = ClientNotificationSetting.objects.get(business = str(appo.appointment.business))
+                except:
+                    pass
+                if staff_email.sms_daily_sale == True:
+                    try:   
+                        html_file = render_to_string("AppointmentEmail/Email_notification_on_appointment_n", {'location':loc_name, 'ser_name':ser_name  , 'duration':dur,'time':datetime.today, 'date':dat, 'staff':staff})
+                        text_content = strip_tags(html_file)
+                            
+                        email = EmailMultiAlternatives(
+                                'Appointment Booked',
+                                text_content,
+                                settings.EMAIL_HOST_USER,
+                                to = [mem_email],
+                            
+                            )
+                        email.attach_alternative(html_file, "text/html")
+                        email.send()
+                    except Exception as err:
+                        pass
+                
+            if client_email.sms_appoinment == True:
+                html_file = render_to_string("AppointmentEmail/Email_notification_on_appointment_n.html",{'name':name_c ,'phone':phon,'email':email_c} )
+                text_content = strip_tags(html_file)
+                
+                email = EmailMultiAlternatives(
+                    'Appointment Booked',
+                    text_content,
+                    settings.EMAIL_HOST_USER,
+                    to = [email_c],
+                )
+                    
+                email.attach_alternative(html_file, "text/html")
+                email.send()
+            
+            ExceptionRecord.objects.create(
+                text = f'create app email {staff_email.sms_daily_sale} {client_email.sms_appoinment}'
+        )
+    
+        except Exception as err:
+            ExceptionRecord.objects.create(
+                text = f'create app email {str(err)} '
+        )
---
+++ Appointment/Constants/cancelappointmentn.py
@@ -0,0 +1,74 @@
+from urllib import response
+from django.template.loader import render_to_string
+from django.core.mail import EmailMultiAlternatives
+from django.utils.html import strip_tags
+from Appointment.models import Appointment, AppointmentService
+
+from django.conf import settings
+from datetime import datetime,date
+from Business.models import ClientNotificationSetting, StaffNotificationSetting
+
+from Utility.models import ExceptionRecord
+from django_tenants.utils import tenant_context
+
+
+def cancel_appointment(appointment = None , tenant = None):
+    if appointment is None or tenant is None:
+        ExceptionRecord.objects.create(
+            text='Appointment, Tenant Is None'
+        )
+
+    with tenant_context(tenant):
+
+        try:
+            appointment =  AppointmentService.objects.get(id = appointment.id)
+            
+            email_c = appointment.appointment.client.email
+            email_m = appointment.member.email
+            mem_id = appointment.member.employee_id
+            name_c = appointment.appointment.client.full_name
+            mem_name = appointment.member.full_name
+
+            ser_name = appointment.service.name
+            dat = appointment.appointment_date
+
+            loc_name = appointment.business_address.address_name
+            dur = appointment.duration
+            phon = appointment.member.mobile_number
+            
+            try:
+                staff_email = StaffNotificationSetting.objects.get(business = str(appointment.appointment.business))
+                client_email = ClientNotificationSetting.objects.get(business = str(appointment.appointment.business))
+            except:
+                pass
+            if staff_email.sms_daily_sale == True:                    
+                html_file = render_to_string("AppointmentEmail/cancel_appointment_n.html", {'location':loc_name, 'ser_name':ser_name  , 'duration':dur,'time':datetime.today, 'date':dat})
+                text_content = strip_tags(html_file)
+                    
+                email = EmailMultiAlternatives(
+                        'Appointment Canceled',
+                        text_content,
+                        settings.EMAIL_HOST_USER,
+                        to = [email_m],
+                    
+                    )
+                email.attach_alternative(html_file, "text/html")
+                email.send()
+                
+            if client_email.sms_appoinment == True:
+                html_file = render_to_string("AppointmentEmail/cancel_appointment_n.html", {'name': name_c,'mobile_number': phon,'email':email_m})
+                text_content = strip_tags(html_file)
+                    
+                email = EmailMultiAlternatives(
+                        'Appointment Canceled',
+                        text_content,
+                        settings.EMAIL_HOST_USER,
+                        to = [email_c],
+                    
+                    )
+                email.attach_alternative(html_file, "text/html")
+                email.send()
+            
+        except Exception as err:
+            print("Appointment error",err)
+    
