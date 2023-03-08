41d2dd04e9416047a1a60c753de3a489768d930e
Abdullah-Ajmal147 <abajmal211@gmail.com>
2023-03-08 12:48:34 +0500

    migration in service package and create new api

--- Client/views/v1/views.py
+++ Client/views/v1/views.py
@@ -2474,7 +2474,7 @@ def get_client_package(request):
         #service_diff = [s.__dict__ for s in list(set(client_service) - set(pac_service)) + list(set(pac_service) - set(client_service))]
 
         service_diff = list(set(client_service) - set(pac_service)) + list(set(pac_service) - set(client_service))
-        Error.append(str(err))
+        Error.append(str(service_diff))
         
         data = serializers.serialize('json', service_diff)
         #data = service_diff.json()
