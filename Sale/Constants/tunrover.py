3e26ff7a228cc73339f8ee9d11338c1171b18397
Abdullah-Ajmal147 <abajmal211@gmail.com>
2023-03-10 11:49:52 +0500

    Turnover email ready

--- Sale/Constants/tunrover.py
+++ Sale/Constants/tunrover.py
@@ -15,38 +15,42 @@ from Business.models import BusinessAddress
 
 def ProductTurnover(product=None, product_stock = None, business_address = None ,tenant= None):
     with tenant_context(tenant):
-        
-        email = 'rijariaz5@gmail.com'
-                
         try:
-            product= Product.objects.get(id = product)
-        except Exception as err:
-            pass
-        try:
-            business= BusinessAddress.objects.get(id = business_address)
-        except Exception as err:
-            pass
-        dates = date.today()
-        
-        # TurnOverProductRecord.objects.create(
-        #     text = 'Email Generate for Product Turnover',
-        #     i_email = 'TURNOVER_PRODUCT'
-        # )
-                
-        try:   
-            html_file = render_to_string("Sales/product_turnover_details.html", {'name': 'Rija Riaz','pro_name': product.name, 'location':business.address_name, 'turnover': 'Lowest' , 'date': dates})
-            text_content = strip_tags(html_file)
-                
-            email = EmailMultiAlternatives(
-                    'Turnover Report',
-                    text_content,
-                    settings.EMAIL_HOST_USER,
-                    to = [email],
-                
+            email = 'rijariaz5@gmail.com'
+                    
+            try:
+                product= Product.objects.get(id = product)
+            except Exception as err:
+                pass
+            try:
+                business= BusinessAddress.objects.get(id = business_address)
+            except Exception as err:
+                pass
+            dates = date.today()
+            
+            # TurnOverProductRecord.objects.create(
+            #     text = 'Email Generate for Product Turnover',
+            #     i_email = 'TURNOVER_PRODUCT'
+            # )
+                    
+            try:   
+                html_file = render_to_string("Sales/product_turnover_details.html", {'name': 'Rija Riaz','pro_name': product.name, 'location':business.address_name, 'turnover': 'Lowest' , 'date': dates})
+                text_content = strip_tags(html_file)
+                    
+                email = EmailMultiAlternatives(
+                        'Turnover Report',
+                        text_content,
+                        settings.EMAIL_HOST_USER,
+                        to = [email],
+                    
+                    )
+                email.attach_alternative(html_file, "text/html")
+                email.send()
+            except Exception as err:
+                ExceptionRecord.objects.create(
+                    text = f'turnover emails error {str(err)}'
                 )
-            email.attach_alternative(html_file, "text/html")
-            email.send()
         except Exception as err:
-            ExceptionRecord.objects.create(
-                text = f'turnover emails error {str(err)}'
-            )
+                ExceptionRecord.objects.create(
+                    text = f'turnover emails error {str(err)}'
+                )
