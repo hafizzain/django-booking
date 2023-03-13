88503c9919164682ee104ba801ca83039899f01d
Abdullah-Ajmal147 <abajmal211@gmail.com>
2023-03-13 15:24:02 +0500

    CHanges in reshedule app

--- Appointment/Constants/Reschedule.py
+++ Appointment/Constants/Reschedule.py
@@ -25,10 +25,11 @@ def reschedule_appointment(appointment = None , tenant = None, client =  None):
             
             appointment =  AppointmentService.objects.filter(appointment = str(appointment))
             current_time = datetime.now().time()
-            
+            client =  False
             for appo in appointment:
                 
                 email_c = appo.appointment.client.email
+                phone = appo.appointment.client.mobile_number
                 name_c = appo.appointment.client.full_name
                 ser_name = appo.service.name
                 dat = appo.appointment_date
@@ -40,6 +41,9 @@ def reschedule_appointment(appointment = None , tenant = None, client =  None):
                 location = appo.appointment.business_address.address_name
                 duration = appo.duration
                 
+                if name is not None:
+                    client = True
+                
                 try:
                     staff_email = StaffNotificationSetting.objects.get(business = str(appo.appointment.business))
                     client_email = ClientNotificationSetting.objects.get(business = str(appo.appointment.business))
@@ -50,7 +54,7 @@ def reschedule_appointment(appointment = None , tenant = None, client =  None):
                         html_file = render_to_string("AppointmentEmail/appointment_reschedule_n.html", {'name': name_c, 
                                     'ser_name':ser_name ,'t_name':mem_name , 
                                     'date':dat, 'mem_id':mem_id,'location': location, 'duration': duration,
-                                    'time': current_time
+                                    'time': current_time, 'email_c': email_c , 'phone': phone,'client': client
                                     })
                         text_content = strip_tags(html_file)
                             
@@ -67,9 +71,26 @@ def reschedule_appointment(appointment = None , tenant = None, client =  None):
                         ExceptionRecord.objects.create(
                             text = f'reschedule_appointment template error{str(err)}'
                         )
+                        
+            if client_email.sms_appoinment == True and client == True:
+                
+                    html_file = render_to_string("AppointmentEmail/appointment_reschedule_n.html", {'name': name_c, 
+                                'ser_name':ser_name ,'t_name':name , 
+                                'date':dat, 'mem_id':mem_id,'location': location, 'duration': duration,
+                                'time': current_time, 'email_c': email_c , 'phone': phone, 'client': client
+                                })
+                    text_content = strip_tags(html_file)
+                        
+                    email = EmailMultiAlternatives(
+                            'Appointment Reschedule',
+                            text_content,
+                            settings.EMAIL_HOST_USER,
+                            to = [email_c],
+                        )
+                    email.attach_alternative(html_file, "text/html")
+                    email.send()
                 
         except Exception as err:
-            #print("Appointment error",err)
             ExceptionRecord.objects.create(
                 text = f'reschedule_appointment {str(err)}'
             )
