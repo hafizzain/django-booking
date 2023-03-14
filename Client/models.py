594f7fa7a00847475324dded02067512387985db
Abdullah-Ajmal147 <abajmal211@gmail.com>
2023-03-14 12:29:05 +0500

    migrations in months membership

--- Client/models.py
+++ Client/models.py
@@ -188,7 +188,7 @@ class Vouchers(models.Model):
     VALIDITY_DAY=[
         ('7 Days' , '7 Days'),
         ('14 Days' , '14 Days'),
-        ('1 Month' ,  '1 Months'),
+        ('1 Months' ,  '1 Months'),
         ('2 Months' , '2 Months'),
         ('3 Months' , '3 Months'),
         ('4 Months' , '4 Months'),
