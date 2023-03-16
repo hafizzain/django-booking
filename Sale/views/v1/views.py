5a3d058caaf30b75ab8ad57bca9179871c344406
Abdullah-Ajmal147 <abajmal211@gmail.com>
2023-03-16 16:34:21 +0500

    migrations in months membership

--- Sale/views/v1/views.py
+++ Sale/views/v1/views.py
@@ -1192,9 +1192,9 @@ def create_sale_order(request):
     product_commission = request.data.get('product_commission', None)
     voucher_commission = request.data.get('voucher_commission', None)
     
-    service_commission_type = request.data.get('service_commission_type', None)
-    product_commission_type = request.data.get('product_commission_type', None)
-    voucher_commission_type = request.data.get('voucher_commission_type', None)
+    service_commission_type = request.data.get('service_commission_type', '')
+    product_commission_type = request.data.get('product_commission_type', '')
+    voucher_commission_type = request.data.get('voucher_commission_type', '')
     
     duration = request.data.get('duration', None)
     
