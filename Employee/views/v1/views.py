89a4b0eb3622394e7b20ff3e1ebc80616544344e
Abdullah-Ajmal147 <abajmal211@gmail.com>
2023-03-20 10:51:38 +0500

    appointment checkout issuee

--- Employee/views/v1/views.py
+++ Employee/views/v1/views.py
@@ -2474,7 +2474,7 @@ def get_employee_commission(request):
     except Exception as err:
         pass
     try:
-        commission = CommissionSchemeSetting.objects.get(employee = str(employe))
+        commission = CommissionSchemeSetting.objects.get(employee = employe)
         serializer = CommissionSerializer(commission, context={'request' : request})
     except Exception as err:
         return Response(
