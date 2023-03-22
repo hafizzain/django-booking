5b0bb452609ab0fda83b5ffd0378b9f03df2f183
Abdullah-Ajmal147 <abajmal211@gmail.com>
2023-03-22 12:06:15 +0500

    appointment checkout issuee

--- Reports/serializers.py
+++ Reports/serializers.py
@@ -7,6 +7,8 @@ from Appointment.serializers import LocationSerializer
 from Business.models import BusinessAddress
 from Employee.models import Employee
 from Product.Constants.index import tenant_media_base_url
+from django.db.models import Sum
+
 
 from Order.models import MemberShipOrder, ProductOrder, ServiceOrder, VoucherOrder
 from Sale.serializers import ProductOrderSerializer
@@ -188,106 +190,155 @@ class ComissionReportsEmployeSerializer(serializers.ModelSerializer):
     service_sale_price = serializers.SerializerMethodField(read_only=True)
     product_sale_price = serializers.SerializerMethodField(read_only=True)
     voucher_sale_price = serializers.SerializerMethodField(read_only=True)
-            
-    def get_product_sale_price(self, obj):
+    
+    def get_product_sale_price(obj, range_start=None, range_end=None, year=None):
         try:
-
-            range_start = self.context["range_start"]
-            range_end = self.context["range_end"]
-            year = self.context["year"] 
-            
-            if range_start:
-                range_start = datetime.strptime(range_start, "%Y-%m-%d").date()
-                range_end = datetime.strptime(range_end, "%Y-%m-%d").date()
-                                          
-            total = 0
-            service_commission = 0
-            
-            product_commission = 0
-            voucher_commission = 0
-            data = {}
-            app   = AppointmentService.objects.filter(
-                member = obj,
-                appointment_status = 'Done',
-                #created_at__icontains = year
+            app = AppointmentService.objects.filter(
+                member=obj,
+                appointment_status='Done',
             )
-            for appointment  in app:                
-                create = str(appointment.created_at)
-                created_at = datetime.strptime(create, "%Y-%m-%d %H:%M:%S.%f%z").date()
-                
-                if range_start:
-                    if range_start >= created_at  and created_at <= range_end:
-                        service_commission += int(appointment.service_commission)
-                else:
-                    service_commission += int(appointment.service_commission)
-                    
-            ExceptionRecord.objects.create(
-                    text=str(service_commission)
-                )
-            
-            product_orders = ProductOrder.objects.filter(
-                is_deleted=False, 
-                member = obj, 
-                )
-            for ord  in product_orders:
-                create = str(ord.created_at)
-                created_at = datetime.strptime(create, "%Y-%m-%d %H:%M:%S.%f%z").date()
-                if range_start is not None:
-                    if created_at >= range_start  and created_at <= range_end:
-                        total += int(ord.checkout.total_service_price)
-                        #service_commission += ord.checkout.service_commission
-                        product_commission += ord.checkout.product_commission
-                        #voucher_commission += ord.checkout.voucher_commission
-        
-                else:
-                    total += int(ord.checkout.total_product_price)
-                    #service_commission += ord.checkout.service_commission
-                    product_commission += ord.checkout.product_commission
-                    #voucher_commission += ord.checkout.voucher_commission
-                    
-            service_orders = ServiceOrder.objects.filter(
-                is_deleted=False, 
-                member = obj, 
-                )
-            
-            for ord  in service_orders:
-                create = str(ord.created_at)
-                created_at = datetime.strptime(create, "%Y-%m-%d %H:%M:%S.%f%z").date()
-                if range_start is not None:
-                    if created_at >= range_start  and created_at <= range_end:
-                        #total += int(ord.checkout.total_service_price)
-                        service_commission += ord.checkout.service_commission
-                    
-                else:
-                    service_commission += ord.checkout.service_commission
-            
-            voucher_orders = VoucherOrder.objects.filter(
-                is_deleted=False, 
-                member = obj, 
-                )
-            for ord  in voucher_orders:
-                create = str(ord.created_at)
-                created_at = datetime.strptime(create, "%Y-%m-%d %H:%M:%S.%f%z").date()
-                if range_start is not None:
-                    if created_at >= range_start  and created_at <= range_end:
-                       # total += int(ord.checkout.total_service_price)
-                        voucher_commission += ord.checkout.voucher_commission
-                else:
-                    #total += int(ord.checkout.total_product_price)
-                    voucher_commission += ord.checkout.voucher_commission
+            if range_start:
+                range_start = datetime.strptime(range_start, '%Y-%m-%d').date()
+                range_end = datetime.strptime(range_end, '%Y-%m-%d').date()
+                app = app.filter(created_at__range=(range_start, range_end))
+            total_service_commission = app.aggregate(Sum('service_commission'))['service_commission__sum'] or 0
 
-            commission_total = service_commission + product_commission + voucher_commission
-            data.update({
-                'product_sale_price': total,
+            product_orders = ProductOrder.objects.filter(
+                is_deleted=False,
+                member=obj,
+            )
+            if range_start:
+                product_orders = product_orders.filter(created_at__range=(range_start, range_end))
+            total_product_price = product_orders.aggregate(Sum('checkout__total_product_price'))['checkout__total_product_price__sum'] or 0
+            product_commission = product_orders.aggregate(Sum('checkout__product_commission'))['checkout__product_commission__sum'] or 0
+
+            service_orders = ServiceOrder.objects.filter(
+                is_deleted=False,
+                member=obj,
+            )
+            if range_start:
+                service_orders = service_orders.filter(created_at__range=(range_start, range_end))
+            service_commission = service_orders.aggregate(Sum('checkout__service_commission'))['checkout__service_commission__sum'] or 0
+
+            voucher_orders = VoucherOrder.objects.filter(
+                is_deleted=False,
+                member=obj,
+            )
+            if range_start:
+                voucher_orders = voucher_orders.filter(created_at__range=(range_start, range_end))
+            voucher_commission = voucher_orders.aggregate(Sum('checkout__voucher_commission'))['checkout__voucher_commission__sum'] or 0
+
+            commission_total = total_service_commission + product_commission + voucher_commission
+            data = {
+                'product_sale_price': total_product_price,
                 'commission_total': commission_total,
                 'service_commission': service_commission,
                 'product_commission': product_commission,
                 'voucher_commission': voucher_commission,
-            })
+            }
             return data
+        except Exception as e:
+            return str(e)
+            
+    # def get_product_sale_price(self, obj):
+    #     try:
+
+    #         range_start = self.context["range_start"]
+    #         range_end = self.context["range_end"]
+    #         year = self.context["year"] 
+            
+    #         if range_start:
+    #             range_start = datetime.strptime(range_start, "%Y-%m-%d").date()
+    #             range_end = datetime.strptime(range_end, "%Y-%m-%d").date()
+                                          
+    #         total = 0
+    #         service_commission = 0
+            
+    #         product_commission = 0
+    #         voucher_commission = 0
+    #         data = {}
+    #         app   = AppointmentService.objects.filter(
+    #             member = obj,
+    #             appointment_status = 'Done',
+    #             #created_at__icontains = year
+    #         )
+    #         for appointment  in app:                
+    #             create = str(appointment.created_at)
+    #             created_at = datetime.strptime(create, "%Y-%m-%d %H:%M:%S.%f%z").date()
                 
-        except Exception as err:
-            return str(err)
+    #             if range_start:
+    #                 if range_start >= created_at  and created_at <= range_end:
+    #                     service_commission += int(appointment.service_commission)
+    #             else:
+    #                 service_commission += int(appointment.service_commission)
+                    
+    #         ExceptionRecord.objects.create(
+    #                 text=str(service_commission)
+    #             )
+            
+    #         product_orders = ProductOrder.objects.filter(
+    #             is_deleted=False, 
+    #             member = obj, 
+    #             )
+    #         for ord  in product_orders:
+    #             create = str(ord.created_at)
+    #             created_at = datetime.strptime(create, "%Y-%m-%d %H:%M:%S.%f%z").date()
+    #             if range_start is not None:
+    #                 if created_at >= range_start  and created_at <= range_end:
+    #                     total += int(ord.checkout.total_service_price)
+    #                     #service_commission += ord.checkout.service_commission
+    #                     product_commission += ord.checkout.product_commission
+    #                     #voucher_commission += ord.checkout.voucher_commission
+        
+    #             else:
+    #                 total += int(ord.checkout.total_product_price)
+    #                 #service_commission += ord.checkout.service_commission
+    #                 product_commission += ord.checkout.product_commission
+    #                 #voucher_commission += ord.checkout.voucher_commission
+                    
+    #         service_orders = ServiceOrder.objects.filter(
+    #             is_deleted=False, 
+    #             member = obj, 
+    #             )
+            
+    #         for ord  in service_orders:
+    #             create = str(ord.created_at)
+    #             created_at = datetime.strptime(create, "%Y-%m-%d %H:%M:%S.%f%z").date()
+    #             if range_start is not None:
+    #                 if created_at >= range_start  and created_at <= range_end:
+    #                     #total += int(ord.checkout.total_service_price)
+    #                     service_commission += ord.checkout.service_commission
+                    
+    #             else:
+    #                 service_commission += ord.checkout.service_commission
+            
+    #         voucher_orders = VoucherOrder.objects.filter(
+    #             is_deleted=False, 
+    #             member = obj, 
+    #             )
+    #         for ord  in voucher_orders:
+    #             create = str(ord.created_at)
+    #             created_at = datetime.strptime(create, "%Y-%m-%d %H:%M:%S.%f%z").date()
+    #             if range_start is not None:
+    #                 if created_at >= range_start  and created_at <= range_end:
+    #                    # total += int(ord.checkout.total_service_price)
+    #                     voucher_commission += ord.checkout.voucher_commission
+    #             else:
+    #                 #total += int(ord.checkout.total_product_price)
+    #                 voucher_commission += ord.checkout.voucher_commission
+
+    #         commission_total = service_commission + product_commission + voucher_commission
+    #         data.update({
+    #             'product_sale_price': total,
+    #             'commission_total': commission_total,
+    #             'service_commission': service_commission,
+    #             'product_commission': product_commission,
+    #             'voucher_commission': voucher_commission,
+    #         })
+    #         return data
+                
+    #     except Exception as err:
+    #         return str(err)
     
     def get_service_sale_price(self, obj):
         try:
