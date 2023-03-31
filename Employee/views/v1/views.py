aa7e73926a0d9ee676c4c43cdb849297871db96c
Abdullah-Ajmal147 <abajmal211@gmail.com>
2023-03-31 20:30:18 +0500

    migrations in sale checkout and new changings

--- Employee/views/v1/views.py
+++ Employee/views/v1/views.py
@@ -3341,8 +3341,10 @@ def create_vacation_emp(request):
                 pass
             
             if working_schedule is not None:
+                date_obj = datetime.fromisoformat(from_date)
+                
                 working_schedule.is_vacation = True
-                working_schedule.from_date =from_date,
+                working_schedule.from_date = date_obj,
                 working_schedule.save()
                 
             else:   
