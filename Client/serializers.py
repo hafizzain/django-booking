046256b0c5f4e206f87524c9827c8f57ecf0347f
HuzaifaYaqoob <zeevhyorg@gmail.com>
2023-04-27 11:48:55 +0500

    loyalty points loged issues fixed

--- Client/serializers.py
+++ Client/serializers.py
@@ -274,20 +274,20 @@ class CustomerDetailedLoyaltyPointsLogsSerializer(serializers.ModelSerializer):
 
     def get_customer(self, c_points):
         return {
-            'customer_id' : f'{c_points.client.client_id}',
-            'customer_name' : f'{c_points.client.full_name}',
+            'customer_id' : f'c_points.client.client_id',
+            'customer_name' : f'c_points.client.full_name',
         }
 
     def get_loyalty(self, c_points):
         return {
-            'loyalty_name' : ''
+            'loyalty_name' : f'c_points.loyalty_points.name'
         }
 
     def get_points_earned(self, c_points):
-        return c_points.total_earn
+        return 'c_points.total_earn'
 
     def get_points_redeemed(self, c_points):
-        return c_points.points_redeemed
+        return 'c_points.points_redeemed'
 
     def get_balance(self, c_points):
         return 0
