004dd7e9c525db92199b404086204b38d16f7388
HuzaifaYaqoob <zeevhyorg@gmail.com>
2023-05-05 10:36:26 +0500

    admin customization

--- Order/admin.py
+++ Order/admin.py
@@ -5,6 +5,13 @@ from Order.models import Order, ProductOrder, ServiceOrder , MemberShipOrder ,Vo
 admin.site.register(ProductOrder)
 admin.site.register(ServiceOrder)
 admin.site.register(MemberShipOrder)
-admin.site.register(VoucherOrder)
+
+
+@admin.register(VoucherOrder)
+class VoucherOrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'start_date', 'end_date']
+
+
+
 admin.site.register(Checkout)
 admin.site.register(CheckoutPayment)
