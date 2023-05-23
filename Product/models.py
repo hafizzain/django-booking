b4c9f6127e83d87992db0613a571d5b9b51e4205
HuzaifaYaqoob <zeevhyorg@gmail.com>
2023-05-23 15:47:32 +0500

    short id

--- Product/models.py
+++ Product/models.py
@@ -280,6 +280,7 @@ class ProductOrderStockReport(models.Model):
     is_deleted = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=now, null= True)
 
+    @property
     def short_id(self):
         instance_id = f'{self.id}'
         instance_id = instance_id.split('-')[0]
