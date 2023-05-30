80b462548a79c96e9bed7e13d19da0a5cc4feb82
Ocman Nazir Briet <usmannazirbareet@gmail.com>
2023-05-30 11:17:34 +0500

    added new apis in helpcenter and new migrations

--- Help/models.py
+++ Help/models.py
@@ -2,10 +2,10 @@ from django.db import models
 import uuid
 
 
-class Comment(models.Model):
+class HelpContent(models.Model):
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
     content = models.TextField()
-    parent_comment = models.ForeignKey('Comment', on_delete=models.CASCADE, null=True, blank=True)
+    parent_comment = models.ForeignKey('HelpContent', on_delete=models.CASCADE, null=True, blank=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
     is_parent = models.BooleanField(default=False)
--- Help/urls/v1/urls.py
+++ Help/urls/v1/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
    path('add_query/', views.add_query),
    path('get_comment/', views.get_comment),
    path('get_comment_details/', views.get_comment_details),
+   path('delete_comment/', views.delete_comment),
+   path('update_comment/', views.update_comment),
 ]
