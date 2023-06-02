f40e8848a7e9a83f50fcef2a84aac2575a1f79bb
Ocman Nazir Briet <usmannazirbareet@gmail.com>
2023-06-02 13:24:28 +0500

    changed folder structure for temolates

--- SuperInsight/views.py
+++ SuperInsight/views.py
@@ -4,19 +4,19 @@ from django.shortcuts import render
 
 
 def DashboardPage(request):
-    return render(request, 'SuperAdminPanel/SuperInsights/dashboard.html')
+    return render(request, 'SuperAdminPanel/pages/dashboard/dashboard.html')
 
 def ExceptionPage(request):
-    return render(request, 'SuperAdminPanel/SuperInsights/exception.html')
+    return render(request, 'SuperAdminPanel/pages/exception/exception.html')
 
 def ExceptionDetailPage(request):
-    return render(request, 'SuperAdminPanel/SuperInsights/exception-detail.html')
+    return render(request, 'SuperAdminPanel/pages/exception/exception-detail.html')
 
 def LanguagePage(request):
-    return render(request, 'SuperAdminPanel/SuperInsights/language.html')
+    return render(request, 'SuperAdminPanel/pages/language/language.html')
 
 def LanguageSectionPage(request):
-    return render(request, 'SuperAdminPanel/SuperInsights/language-section.html')
+    return render(request, 'SuperAdminPanel/pages/language/language-section.html')
 
 def LanguageSectionDetailPage(request):
-    return render(request, 'SuperAdminPanel/SuperInsights/language-section-detail.html')
+    return render(request, 'SuperAdminPanel/pages/language/language-section-detail.html')
