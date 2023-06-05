720a22cdac71b175530703bc4cd8e8a6ed664bb8
Ocman Nazir Briet <usmannazirbareet@gmail.com>
2023-06-05 15:59:34 +0500

    added new app MultiLanguages and new migrations

--- MultiLanguage/urls/v1/urls.py
+++ MultiLanguage/urls/v1/urls.py
@@ -1,10 +1,10 @@
-# from django.urls import path, include
-# from MultiLanguage import views
+from django.urls import path, include
+from MultiLanguage import views
 
-# urlpatterns = [
-#    path('add_language/', views.add_language),
-#    path('add_section/', views.add_section, name="add_section"),
-#    path('add_labels/', views.add_labels),
-#    path('get_data/', views.get_data),
-# ]
+urlpatterns = [
+   path('add_language/', views.add_language),
+   path('add_section/', views.add_section, name="add_section"),
+   path('add_labels/', views.add_labels),
+   path('get_data/', views.get_data),
+]
 
