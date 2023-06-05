0ca099c7fccb71df60edda28617321c03dd22cda
HuzaifaYaqoob <zeevhyorg@gmail.com>
2023-06-05 11:45:13 +0500

    filtered insight api customization and created at field added

--- Product/views/v1/insights.py
+++ Product/views/v1/insights.py
@@ -11,40 +11,61 @@ from django.db.models import Count
 from Product.serializers import ProductInsightSerializer
 import re
 
-from datetime import datetime
+from datetime import datetime, timedelta
 
 DATE_REGEX = '\d{4}-\d{2}-\d{2}'
 
 class FilteredInsightProducts(APIView):
     permission_classes = [AllowAny]
 
-    def get(self, request):
+    today_date = datetime.now()
+    today_date_format = today_date.strftime('%Y-%m-%d')
+    days_before_7 = (today_date - timedelta(days=7)).strftime('%Y-%m-%d')
+    days_before_30 = (today_date - timedelta(days=30)).strftime('%Y-%m-%d')
 
+    queries = {
+        'filter' : {},'order_by' : [], 'annotate' : {}
+    }
+
+    def retreive_top_sold_query(self, request):
         top_sold = request.GET.get('top_sold', None)
-        TOP_SOLD_CHOICES = {'TOP_SOLD_PRODUCTS' : lambda : None, 'LAST_7_DAYS' : lambda : datetime.date().strftime('%Y-%') , 'LAST_30_DAYS' : '' }
-
-        most_consumed = request.GET.get('most_consumed', None)
-        MOST_COMSUMED_CHOICES = ['MOST_COMSUMED_PRODUCTS', 'LAST_7_DAYS', 'LAST_30_DAYS']
-
-        most_ordered = request.GET.get('most_ordered', None)
-        MOST_ORDERED_CHOICES = ['MOST_ORDERED_PRODUCTS', 'LAST_7_DAYS', 'LAST_30_DAYS']
-
-        most_transferred = request.GET.get('most_transferred', None)
-        MOST_TRANSFERRED_CHOICES = ['MOST_TRANSFERRED_PRODUCTS', 'LAST_7_DAYS', 'LAST_30_DAYS']
-
-        low_stock_products = request.GET.get('low_stock_products', None)
-        out_of_stock_products = request.GET.get('out_of_stock_products', None)
-
-
-        queries = {
-            'filter' : {},'order_by' : [], 'annotate' : {}
-        }
+        TOP_SOLD_CHOICES = {'TOP_SOLD_PRODUCTS' : lambda : None, 'LAST_7_DAYS' : self.days_before_30 , 'LAST_30_DAYS' : self.days_before_30 }
 
         if top_sold :
-            queries['order_by'].append('-top_sold_orders')
-            queries['annotate']['top_sold_orders'] = Count('product_orders__quantity')
+            self.queries['order_by'].append('-top_sold_orders')
+            self.queries['annotate']['top_sold_orders'] = Count('product_orders__quantity')
             if top_sold in TOP_SOLD_CHOICES or re.match(DATE_REGEX, top_sold):
-                pass
+                if top_sold != 'TOP_SOLD_PRODUCTS':
+                    value = top_sold
+                    if value in ['LAST_7_DAYS', 'LAST_30_DAYS']:
+                        value = TOP_SOLD_CHOICES.get(value)
+                    self.queries['filter']['product_orders__created_at__range'] = (value, self.today_date_format)
+            else:
+                return Response(
+                    {
+                        'status' : False,
+                        'status_code' : 400,
+                        'response' : {
+                            'message' : 'Invalid Top Sold Query',
+                            'error_message' : None,
+                        }
+                    },
+                    status=status.HTTP_200_OK
+                )
+            
+    def retreive_most_consumed_query(self, request):
+        most_consumed = request.GET.get('most_consumed', None)
+        MOST_COMSUMED_CHOICES = {'MOST_COMSUMED_PRODUCTS' : lambda : None, 'LAST_7_DAYS' : self.days_before_30 , 'LAST_30_DAYS' : self.days_before_30 }
+
+        if most_consumed :
+            self.queries['order_by'].append('-most_consumed_products')
+            self.queries['annotate']['most_consumed_products'] = Count('consumptions__quantity')
+            if most_consumed in MOST_COMSUMED_CHOICES or re.match(DATE_REGEX, most_consumed):
+                if most_consumed != 'MOST_COMSUMED_PRODUCTS':
+                    value = most_consumed
+                    if value in ['LAST_7_DAYS', 'LAST_30_DAYS']:
+                        value = MOST_COMSUMED_CHOICES.get(value)
+                    self.queries['filter']['consumptions__created_at__range'] = (value, self.today_date_format)
             else:
                 return Response(
                     {
@@ -58,12 +79,32 @@ class FilteredInsightProducts(APIView):
                     status=status.HTTP_200_OK
                 )
 
+
+    def get(self, request):
+
+        most_consumed = request.GET.get('most_consumed', None)
+        MOST_COMSUMED_CHOICES = ['MOST_COMSUMED_PRODUCTS', 'LAST_7_DAYS', 'LAST_30_DAYS']
+
+        most_ordered = request.GET.get('most_ordered', None)
+        MOST_ORDERED_CHOICES = ['MOST_ORDERED_PRODUCTS', 'LAST_7_DAYS', 'LAST_30_DAYS']
+
+        most_transferred = request.GET.get('most_transferred', None)
+        MOST_TRANSFERRED_CHOICES = ['MOST_TRANSFERRED_PRODUCTS', 'LAST_7_DAYS', 'LAST_30_DAYS']
+
+        low_stock_products = request.GET.get('low_stock_products', None)
+        out_of_stock_products = request.GET.get('out_of_stock_products', None)
+        
+
+        
+        self.retreive_top_sold_query(request)
+        self.retreive_most_consumed_query(request)
+
         filtered_products = Product.objects.filter(
             is_deleted = False,
-            **queries['filter'],
+            **self.queries['filter'],
         ).annotate(
-            **queries['annotate'],
-        ).distinct().order_by(*queries['order_by'])
+            **self.queries['annotate'],
+        ).distinct().order_by(*self.queries['order_by'])
 
         # serialized = ProductInsightSerializer(filtered_products, many=True)
 
@@ -75,15 +116,18 @@ class FilteredInsightProducts(APIView):
                 'name' : f'{product_instance.name}',
             }
 
-            if product_instance.top_sold_orders:
-                product['top_sold_orders'] = f'{product_instance.top_sold_orders}'
+            if top_sold:
+                product['top_sold_orders'] = int(product_instance.top_sold_orders)
 
             data.append(product)
 
-        return Response(
+        response = Response(
             {
                 'status' : True,
                 'status_code' : 200,
+                'request' : {
+                    'queries' : self.queries
+                },
                 'response' : {
                     'message' : 'Insight Products',
                     'error_message' : None,
@@ -92,3 +136,4 @@ class FilteredInsightProducts(APIView):
             },
             status=status.HTTP_200_OK
         )
+        return response
