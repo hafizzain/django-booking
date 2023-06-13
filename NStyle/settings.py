99d25f6c8602b5fcbce7e2987448aaa70c37a70d
HuzaifaYaqoob <zeevhyorg@gmail.com>
2023-06-13 18:05:35 +0500

    custom auth backend

--- NStyle/settings.py
+++ NStyle/settings.py
@@ -175,10 +175,10 @@ FILE_UPLOAD_MAX_MEMORY_SIZE = 10485760
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
-# AUTHENTICATION_BACKENDS = (
-#     'NStyle.CustomerAuthBackend.CustomerAuthBackend',
-#     'django.contrib.auth.backends.ModelBackend',
-# )
+AUTHENTICATION_BACKENDS = (
+    'NStyle.CustomerAuthBackend.CustomerAuthBackend',
+    'django.contrib.auth.backends.ModelBackend',
+)
 
 AUTH_PASSWORD_VALIDATORS = [
     {
