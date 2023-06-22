886f1fd9ad620c5058bbfb88f63a47229007c347
HuzaifaYaqoob <zeevhyorg@gmail.com>
2023-06-22 12:31:13 +0500

    services__serivce_appointments__appointment_status

--- Reports/views/v1/views.py
+++ Reports/views/v1/views.py
@@ -25,7 +25,7 @@ from Business.models import BusinessAddress
 from Service.models import PriceService, Service, ServiceGroup
 
 from Product.models import Brand, Product, ProductOrderStockReport, ProductStock
-from django.db.models import Avg, Count, Min, Sum, F, FloatField
+from django.db.models import Avg, Count, Min, Sum, F, FloatField, Q
 
 
 from Sale.serializers import AppointmentCheckout_ReportsSerializer, PromotionNDiscount_AppointmentCheckoutSerializer, PromotionNDiscount_CheckoutSerializer, MemberShipOrderSerializer, ProductOrderSerializer, ServiceGroupSerializer, ServiceOrderSerializer, ServiceSerializer, VoucherOrderSerializer, CheckoutCommissionSerializer
@@ -180,7 +180,7 @@ def get_service_target_report(request):
         is_deleted = False,
     ).annotate(
         services_sales = Sum(F('services__service_orders__quantity') * F('services__service_orders__total_price'), output_field=FloatField()),
-        appointment_sales = Sum(F('services__serivce_appointments__price'), output_field=FloatField()),
+        appointment_sales = Sum(F('services__serivce_appointments__price'), filter=Q(services__serivce_appointments__appointment_status = 'Done'), output_field=FloatField()),
         # total_service_sales = Sum(F('services_sales'), F('appointment_sales'))
     ).order_by('-created_at')
     serialized = ServiceGroupReport(address, many=True, context={'request' : request, 
