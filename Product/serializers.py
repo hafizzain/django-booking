363795194ae5a354cf247ebea37247510f6e62c2
Ocman Nazir Briet <usmannazirbareet@gmail.com>
2023-06-24 18:18:58 +0500

    fixing stock report zero issue

--- Product/serializers.py
+++ Product/serializers.py
@@ -557,7 +557,8 @@ class ProductStockReportSerializer(serializers.ModelSerializer):
         return f'{product_instance.created_at.strftime("%Y-%m-%d")}'
 
     def get_stock(self, product_instance):
-        stock = ProductStock.objects.filter(product=product_instance, is_deleted=False)#[0]
+        location_id = self.context.get('location_id')
+        stock = ProductStock.objects.filter(product=product_instance, is_deleted=False, location=location_id)#[0]
         return ProductInventoryStockSerializer(stock, many = True).data
     
     def get_brand(self, obj):
