3c2fd6a16e743590276a5df73d864c95d1f85de8
HuzaifaYaqoob <zeevhyorg@gmail.com>
2023-07-05 12:19:28 +0500

    model registerr

--- Reports/admin.py
+++ Reports/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
-
+from .models import DiscountPromotionSalesReport
 # Register your models here.
+
+
+@admin.register(DiscountPromotionSalesReport)
+class DiscountPromotionSalesReportAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'checkout_type',
+        'invoice',
+        'promotion_id',
+        'promotion_type',
+        'promotion_name',
+        'location',
+        'gst',
+        'original_price',
+        'discount_price',
+    ]
