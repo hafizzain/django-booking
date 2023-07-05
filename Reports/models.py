31b6b428ef31842a933aa705367f786e889be799
HuzaifaYaqoob <zeevhyorg@gmail.com>
2023-07-05 23:17:35 +0500

    discounted and original prices for appointment

--- Reports/models.py
+++ Reports/models.py
@@ -7,7 +7,7 @@ from Business.models import BusinessAddress
 from Invoices.models import SaleInvoice
 from Sale.Constants.Promotion import get_promotions
 from Order.models import Checkout, Order, ServiceOrder, ProductOrder
-from Appointment.models import AppointmentCheckout
+from Appointment.models import AppointmentCheckout, AppointmentService
 from Service.models import PriceService
 from Product.models import CurrencyRetailPrice
 # Create your models here.
@@ -57,6 +57,32 @@ def get_ItemPrice(this_instance, orders):
     discounted_prices = d1 + (o1 - d2 - f1)
     return [o1, discounted_prices]
 
+def get_Appointment_ItemPrice(this_instance, orders):
+    o1 = 0
+    d1 = 0
+    d2 = 0
+    f1 = 0
+
+    for order in orders:
+        price = order.discount_price or order.total_price
+        
+        if price > 0:
+            if order.discount_price:
+                d1 += float(order.discount_price)
+                d2 += float(order.total_price)
+
+            o1 += float(order.total_price)
+        else:
+            service_prices = PriceService.objects.filter(service = order.service, duration = order.duration, currency = this_instance.business_address.currency)
+            if len(service_prices) > 0:
+                service_price = service_prices[0].price
+                f1 += float(service_price)
+                o1 += float(service_price)
+
+    discounted_prices = d1 + (o1 - d2 - f1)
+    return [o1, discounted_prices]
+
+
 def get_fixed_prices(this_instance, orders):
     original_price = 0
     discounted_prices = 0
@@ -87,6 +113,25 @@ def get_fixed_prices(this_instance, orders):
                 original_price += float(service_price) * float(order.quantity)
 
     return [original_price, discounted_prices]
+
+
+def get_Appointment_fixed_prices(this_instance, orders):
+    original_price = 0
+    discounted_prices = 0
+
+    for order in orders:
+        
+        discounted_prices += float(order.total_price)
+       
+        service_prices = PriceService.objects.filter(service = order.service, duration = order.duration, currency = this_instance.business_address.currency)
+        if len(service_prices) > 0:
+            service_price = service_prices[0].price
+            original_price += float(service_price)
+
+    return [original_price, discounted_prices]
+
+
+
 class DiscountPromotionSalesReport(models.Model):
 
     CHECKOUT_TYPES = (
@@ -125,6 +170,40 @@ class DiscountPromotionSalesReport(models.Model):
     def get_appointment_prices(self):
         original_prices = 0
         discounted_prices = 0
+
+        app_checkout = AppointmentCheckout.objects.get(
+            id = self.checkout_id
+        )
+
+        orders = AppointmentService.objects.filter(
+            appointment = app_checkout.appointment
+        )
+
+            
+        if self.promotion_type == 'Purchase Discount':
+            original_prices, discounted_prices = get_Appointment_ItemPrice(self, orders)
+
+        elif self.promotion_type == 'Spend_Some_Amount':
+            original_prices, discounted_prices = get_Appointment_ItemPrice(self, orders)
+
+        elif self.promotion_type == 'Mentioned_Number_Service':
+            original_prices, discounted_prices = get_Appointment_ItemPrice(self, orders)
+
+        elif self.promotion_type == 'Fixed_Price_Service':
+            original_prices, discounted_prices = get_Appointment_fixed_prices(self, orders)
+
+        elif self.promotion_type == 'Bundle_Fixed_Service':
+            original_prices, discounted_prices = get_Appointment_fixed_prices(self, orders)
+
+        elif self.promotion_type == 'Retail_and_Get_Service':
+            pass
+
+        else:
+            for order in orders:
+                if order.discount_price:
+                    discounted_prices += float(order.discount_price)
+
+                original_prices += float(order.total_price)
 
         return {
             'original_prices' : original_prices,
