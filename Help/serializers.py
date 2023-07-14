f80fd8007303fe44e3d8e3550d932bc4cff2b874
Ocman Nazir Briet <usmannazirbareet@gmail.com>
2023-07-14 18:25:59 +0500

    returned image  in help center serial

--- Help/serializers.py
+++ Help/serializers.py
@@ -4,4 +4,4 @@ from .models import HelpContent
 class HelpContentSerializer(serializers.ModelSerializer):
     class Meta:
         model = HelpContent
-        fields = ['id', 'content', 'parent_comment', 'is_parent']
+        fields = ['id', 'content', 'parent_comment', 'is_parent', 'image']
