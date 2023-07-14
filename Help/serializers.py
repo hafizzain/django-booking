2b5d2bd343a5ad30adf4626d22429a3cca3c7dca
Ocman Nazir Briet <usmannazirbareet@gmail.com>
2023-07-14 16:30:43 +0500

    added id in get comment in help center serial

--- Help/serializers.py
+++ Help/serializers.py
@@ -4,4 +4,4 @@ from .models import HelpContent
 class HelpContentSerializer(serializers.ModelSerializer):
     class Meta:
         model = HelpContent
-        fields = ['content', 'parent_comment', 'is_parent']
+        fields = ['id', 'content', 'parent_comment', 'is_parent']
