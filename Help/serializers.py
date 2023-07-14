d859f0a51b86448d761387fd7659dde2158b8d07
Ocman Nazir Briet <usmannazirbareet@gmail.com>
2023-07-14 18:43:29 +0500

    returned image  in help center serial

--- Help/serializers.py
+++ Help/serializers.py
@@ -1,20 +1,7 @@
 from rest_framework import serializers
 from .models import HelpContent
-from Product.Constants.index import tenant_media_base_url
 
 class HelpContentSerializer(serializers.ModelSerializer):
-    imagee = serializers.SerializerMethodField()
-    
-    def get_imagee(self, obj):
-        if obj.image:
-            try:
-                request = self.context["request"]
-                url = tenant_media_base_url(request)
-                return f'{url}{obj.image}'
-            except:
-                return obj.image
-        return ''
-    
     class Meta:
         model = HelpContent
-        fields = ['id', 'content', 'parent_comment', 'is_parent', 'imagee']
+        fields = ['id', 'content', 'parent_comment', 'is_parent', 'image']
