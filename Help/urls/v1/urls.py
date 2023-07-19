dbe9a87b2213cbcee74b9a9f725142a15b04ef50
ocman-nazir-briet <usmannazirbareet@gmail.com>
2023-07-19 13:36:11 +0500

    added help center content adding scanerio

--- Help/urls/v1/urls.py
+++ Help/urls/v1/urls.py
@@ -3,9 +3,14 @@ from django.urls import path, include
 from Help import views
 
 urlpatterns = [
-   path('add_query/', views.add_query),
+   # path('add_query/', views.add_query),
    path('get_comment/', views.get_comment),
    path('get_comment_details/', views.get_comment_details),
-   path('delete_comment/', views.delete_comment),
-   path('update_comment/', views.update_comment),
+   # path('delete_comment/', views.delete_comment),
+   # path('update_comment/', views.update_comment),
+   path('view_content/', views.view_content, name='view_content'),
+   path('add_content/', views.add_content),
+   path('add_topic_content/', views.add_topic_content),
+   path('view_topic_content/', views.view_topic_content),
+
 ]
--- Help/views.py
+++ Help/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
@@ -8,72 +8,71 @@ from .serializers import *
 import json
 
 
-@api_view(['POST'])
-@permission_classes([AllowAny])
-def add_query(request):
-    content = request.data.get('content')
-    parent_comment = request.data.get('parent_comment', None)
-    print(parent_comment)
 
-    if content is None:
-        return Response(
-            {
-                'success':False,
-                'status_code':400,
-                'status_code_text' : '400',
-                'response':
-                {
-                    'message':'Invalid data',
-                }
-            },
-            status=status.HTTP_400_BAD_REQUEST
-        )
 
-    comment_obj = HelpContent.objects.create(content=content)
 
-    if parent_comment is not None:
-
-        try:
-            parent = HelpContent.objects.get(id = parent_comment)
-            print(parent)
-        except Exception as e:
-            return Response(
-                {
-                    'success':False,
-                    'message':'Data Not Found, Invalid Parent ID',
-                    'status_code':400,
-                    'status_code_text' : '400',
-                    'response':
-                    {
-                        'message':str(e),
-                    }
-                },
-                status=status.HTTP_404_NOT_FOUND
-            )
+def add_content(request):
+    if request.method == 'POST':
+        topic = request.POST.get('topic', None)
+        desc = request.POST.get('desc', None)
+        image = request.FILES.get('image')
+        is_recent = request.POST.get('is_recent', False)
         
-        parent.is_parent = True
-        parent.save()
-        comment_obj.parent_comment = parent
-        comment_obj.save()
-    else:
-        comment_obj.is_parent = True
-        comment_obj.save()
+        if is_recent=='on':
+            is_recent_check = True
+        else:
+            is_recent_check = False
+
+        print("*********************", image)
+        help_content = HelpContent.objects.create(
+            content=topic, 
+            description = desc, 
+            image = image,
+            is_recent = is_recent_check,
+            is_parent = True
+            )
+        help_content.save()
+        return redirect('view_content')
+    return redirect('view_content')
 
 
-    return Response(
-        {
-            'success':True,
-            'status_code':200,
-            'status_code_text' : '200',
-            'response':
-            {
-                'message':'Created Successfully',
-                'response':str(comment_obj),
-                'error_message': None
-            }
-        },
-        status=status.HTTP_200_OK
-    )
+
+def view_content(request):
+    content = HelpContent.objects.filter(is_parent = True, parent_comment__isnull = True)
+    context={}
+    context['content'] = content
+    return render(request, 'help/help-center.html', context)
+
+
+def add_topic_content(request):
+    if request.method == 'POST':
+        topic = request.POST.get('topic', None)
+        desc = request.POST.get('desc', None)
+        parent_id = request.POST.get('parent', None)
+
+        help_content = HelpContent.objects.create(
+            content=topic, 
+            description = desc,
+            )
+        parent = HelpContent.objects.get(id=parent_id)
+        help_content.parent_comment = parent
+        help_content.save()
+        return redirect(f'/api/v1/help/view_topic_content/?id={parent_id}')
+    return redirect(f'/api/v1/help/view_topic_content/?id={parent_id}')
+
+
+
+def view_topic_content(request):    
+    id = request.GET.get('id')
+
+    all = HelpContent.objects.filter(parent_comment=id).order_by('content')
+    context={}
+    context['content'] = all
+    context['parent'] = id
+    return render(request, 'help/help-center-topics.html', context)
+
+
+
 
 
 @api_view(['GET'])
@@ -160,84 +159,177 @@ def get_comment_details(request):
     )
 
 
-@api_view(['POST'])
-@permission_classes([AllowAny])
-def delete_comment(request):
-    id = request.data.get('id')
-
-    try:
-        comment = HelpContent.objects.get(id=id)
-    except Exception as e:
-        return Response(
-            {
-                'success':False,
-                'message':'Not Found',
-                'status_code':400,
-                'status_code_text' : '400',
-                'response':
-                {
-                    'message':str(e),
-                }
-            },
-        status=status.HTTP_404_NOT_FOUND
-        )
-
-    comment.delete()
-
-    return Response(
-        {
-            'success':True,
-            'message':'Deleted Successfully',
-        },
-        status=status.HTTP_200_OK
-    )
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
-def update_comment(request):
-    id = request.data.get('id')
-    content = request.data.get('content')
+def search_topic(request):
+    topic = request.data.get('topic')
 
-    if content is None:
-        return Response(
-            {
-                'success':False,
-                'status_code':400,
-                'status_code_text' : '400',
-                'response':
-                {
-                    'message':'Invalid data',
-                }
-            },
-            status=status.HTTP_400_BAD_REQUEST
-        )
+    HelpContent.objects.filter()
+
+    
 
 
-    try:
-        comment = HelpContent.objects.get(id=id)
-    except Exception as e:
-        return Response(
-            {
-                'success':False,
-                'message':'Data Not Found',
-                'status_code':400,
-                'status_code_text' : '400',
-                'response':
-                {
-                    'message':str(e),
-                }
-            },
-        status=status.HTTP_404_NOT_FOUND
-        )
 
-    comment.content = content
-    comment.save()
 
-    return Response(
-        {
-            'success':True,
-            'message':'Updated Successfully',
-        },
-        status=status.HTTP_200_OK
-    )
+
+
+
+
+
+
+# @api_view(['POST'])
+# @permission_classes([AllowAny])
+# def add_query(request):
+#     content = request.data.get('content')
+#     parent_comment = request.data.get('parent_comment', None)
+#     print(parent_comment)
+
+#     if content is None:
+#         return Response(
+#             {
+#                 'success':False,
+#                 'status_code':400,
+#                 'status_code_text' : '400',
+#                 'response':
+#                 {
+#                     'message':'Invalid data',
+#                 }
+#             },
+#             status=status.HTTP_400_BAD_REQUEST
+#         )
+
+#     comment_obj = HelpContent.objects.create(content=content)
+
+#     if parent_comment is not None:
+
+#         try:
+#             parent = HelpContent.objects.get(id = parent_comment)
+#             print(parent)
+#         except Exception as e:
+#             return Response(
+#                 {
+#                     'success':False,
+#                     'message':'Data Not Found, Invalid Parent ID',
+#                     'status_code':400,
+#                     'status_code_text' : '400',
+#                     'response':
+#                     {
+#                         'message':str(e),
+#                     }
+#                 },
+#                 status=status.HTTP_404_NOT_FOUND
+#             )
+        
+#         parent.is_parent = True
+#         parent.save()
+#         comment_obj.parent_comment = parent
+#         comment_obj.save()
+#     else:
+#         comment_obj.is_parent = True
+#         comment_obj.save()
+
+
+#     return Response(
+#         {
+#             'success':True,
+#             'status_code':200,
+#             'status_code_text' : '200',
+#             'response':
+#             {
+#                 'message':'Created Successfully',
+#                 'response':str(comment_obj),
+#                 'error_message': None
+#             }
+#         },
+#         status=status.HTTP_200_OK
+#     )
+
+
+
+
+
+
+
+
+# @api_view(['POST'])
+# @permission_classes([AllowAny])
+# def delete_comment(request):
+#     id = request.data.get('id')
+
+#     try:
+#         comment = HelpContent.objects.get(id=id)
+#     except Exception as e:
+#         return Response(
+#             {
+#                 'success':False,
+#                 'message':'Not Found',
+#                 'status_code':400,
+#                 'status_code_text' : '400',
+#                 'response':
+#                 {
+#                     'message':str(e),
+#                 }
+#             },
+#         status=status.HTTP_404_NOT_FOUND
+#         )
+
+#     comment.delete()
+
+#     return Response(
+#         {
+#             'success':True,
+#             'message':'Deleted Successfully',
+#         },
+#         status=status.HTTP_200_OK
+#     )
+
+# @api_view(['POST'])
+# @permission_classes([AllowAny])
+# def update_comment(request):
+#     id = request.data.get('id')
+#     content = request.data.get('content')
+
+#     if content is None:
+#         return Response(
+#             {
+#                 'success':False,
+#                 'status_code':400,
+#                 'status_code_text' : '400',
+#                 'response':
+#                 {
+#                     'message':'Invalid data',
+#                 }
+#             },
+#             status=status.HTTP_400_BAD_REQUEST
+#         )
+
+
+#     try:
+#         comment = HelpContent.objects.get(id=id)
+#     except Exception as e:
+#         return Response(
+#             {
+#                 'success':False,
+#                 'message':'Data Not Found',
+#                 'status_code':400,
+#                 'status_code_text' : '400',
+#                 'response':
+#                 {
+#                     'message':str(e),
+#                 }
+#             },
+#         status=status.HTTP_404_NOT_FOUND
+#         )
+
+#     comment.content = content
+#     comment.save()
+
+#     return Response(
+#         {
+#             'success':True,
+#             'message':'Updated Successfully',
+#         },
+#         status=status.HTTP_200_OK
+#     )
 
