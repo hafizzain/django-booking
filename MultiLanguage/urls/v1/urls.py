79fa533d4a8f65aa56d44fdd79f6ad16e6e086d9
ocman-nazir-briet <usmannazirbareet@gmail.com>
2023-07-20 18:13:57 +0500

    fixed speliiing of invoice in invocie trans

--- MultiLanguage/urls/v1/urls.py
+++ MultiLanguage/urls/v1/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
    path('add_section/', views.add_section, name="add_section"),
    path('add_labels/', views.add_labels),
    path('get_data/', views.get_data),
-   path('add_invocieTranslation/', views.add_invoiceTranslation),
+   path('add_invoiceTranslation/', views.add_invoiceTranslation),
    path('get_invoiceTranslation/', views.get_invoiceTranslation),
 ]
 
