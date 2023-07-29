96808968fa4e16c00ddf74394b72e65876ff0715
ocman-nazir-briet <usmannazirbareet@gmail.com>
2023-07-29 16:11:31 +0500

    fixed exception error

--- SuperInsight/views.py
+++ SuperInsight/views.py
@@ -6,7 +6,7 @@ def DashboardPage(request):
     return render(request, 'SuperAdminPanel/pages/dashboard/dashboard.html')
 
 def ExceptionPage(request):
-    exceptions = ExceptionRecord.objects.all().order_by('-id')
+    exceptions = ExceptionRecord.objects.all().order_by('-created_at')
     context={}
     context['exceptions'] = exceptions
     return render(request, 'SuperAdminPanel/pages/Exception/exception.html', context)
