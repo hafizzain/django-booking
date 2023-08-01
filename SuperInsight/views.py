129e02681f58c85652e6fda4e85b79bb37e31ee6
ocman-nazir-briet <usmannazirbareet@gmail.com>
2023-08-01 11:40:19 +0500

    added lofgin functionality for superadmn

--- SuperInsight/views.py
+++ SuperInsight/views.py
@@ -5,14 +5,16 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.core.exceptions import ObjectDoesNotExist
 
 
 
-@login_required(login_url='/admin')
+
+@login_required(login_url='/super-admin/super-login/')
 def DashboardPage(request):
     return render(request, 'SuperAdminPanel/pages/dashboard/dashboard.html')
 
-@login_required(login_url='/admin')
+@login_required(login_url='/super-admin/super-login/')
 def ExceptionPage(request):
     exceptions = ExceptionRecord.objects.all().order_by('-created_at')
     context={}
@@ -20,7 +22,7 @@ def ExceptionPage(request):
     return render(request, 'SuperAdminPanel/pages/Exception/exception.html', context)
 
 
-@login_required(login_url='/admin')
+@login_required(login_url='/super-admin/super-login/')
 def ExceptionDetailPage(request):
     if request.method == 'GET':
         id = request.GET.get('id')
@@ -32,7 +34,7 @@ def ExceptionDetailPage(request):
     return render(request, 'SuperAdminPanel/pages/Exception/exception-detail.html', context)
 
 
-@login_required(login_url='/admin')
+@login_required(login_url='/super-admin/super-login/')
 def LanguagePage(request):
     languages = Language.objects.all()
     context = {}
@@ -40,7 +42,7 @@ def LanguagePage(request):
     return render(request, 'SuperAdminPanel/pages/language/language.html', context)
 
 
-@login_required(login_url='/admin')
+@login_required(login_url='/super-admin/super-login/')
 def LanguageSectionPage(request):
     lang = request.GET.get('language')
     sections = Section.objects.filter(language__title=lang)
@@ -48,7 +50,7 @@ def LanguageSectionPage(request):
     return render(request, 'SuperAdminPanel/pages/language/language-section.html', {'sections':sections, 'language':lang})
 
 
-@login_required(login_url='/admin')
+@login_required(login_url='/super-admin/super-login/')
 def LanguageSectionDetailPage(request):
     lang = request.GET.get('language')
     section = request.GET.get('section')
@@ -65,31 +67,30 @@ def LanguageSectionDetailPage(request):
 
 def Logout(request):
     logout(request)
-    return redirect('/admin')
+    return redirect('/super-admin/super-login/')
 
 def SuperLogin(request):
     if request.method == "POST":
-        username = request.data.get('username', None)
-        password = request.data.get('password', None)
-    else:
-        pass
+        email = request.POST.get('email', None)
+        password = request.POST.get('password', None)
 
-    try:
-        user = User.objects.get(
-                username=username,
+        try:
+            user = User.objects.get(
+                email=email,
                 is_deleted=False,
                 is_admin=True,
-                user_account_type__account_type = 'Everyone'
             )
-    except:
-        messages.error(request, 'Invalid Username')
-    
-    user = authenticate(request, username=username, password=password)
+        except ObjectDoesNotExist:
+            messages.error(request, 'Invalid credentials')
+            return render(request, 'SuperAdminPanel/pages/dashboard/login.html')
+        
+        username=user.username
+        user = authenticate(request, username=username, password=password)
+        if user is None:
+            messages.error(request, 'Invalid credentials')
+            return render(request, 'SuperAdminPanel/pages/dashboard/login.html')
 
-    if user:    
         login(request, user)
-        messages.success(request, 'Logged in Successfully')
-        return redirect('super-admin/admin')
-    else:
-        messages.error(request, 'Invalid Credentials')
+        return redirect('/super-admin/admin')
+
     return render(request, 'SuperAdminPanel/pages/dashboard/login.html')
