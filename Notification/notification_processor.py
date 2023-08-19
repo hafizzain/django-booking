e0f2f86708214e988cc47478c124d045be6bee01
Moin Abbas <moin.malik996@gmail.com>
2023-08-19 13:07:02 +0500

    some python problems

--- Notification/notification_processor.py
+++ Notification/notification_processor.py
@@ -11,7 +11,7 @@ class NotificationProcessor:
     """
 
     @classmethod
-    def send_notification_per_user(user, title, body):
+    def send_notification_per_user(user=None, title=None, body=None):
         """
         args:
          - user: Employee User
@@ -52,7 +52,7 @@ class NotificationProcessor:
                 )
         else: # single user
             NotificationProcessor.send_notification_per_user(
-                user=users,
-                title=title,
+                user=users
+                title=title
                 body=body
             )
