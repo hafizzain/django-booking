a164b7e7bc45bd41cf0486636b6ae1c1f9536f39
abdullahaslam0079 <abdullah.aslam15327@gmail.com>
2023-08-26 00:17:11 +0500

    signal build for logo upload

---
+++ Business/signals.py
@@ -0,0 +1,13 @@
+
+
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from Utility.Constants.compressImage import upload_to_bucket
+
+from Business.models import Business
+
+@receiver(post_save, sender=Business)
+def UploadLogoToS3Bucket(sender, instance, *args, **kwargs):
+    upload_to_bucket(instance.logo.path, instance.logo.name)
+    instance.is_logo_uploaded_s3 = True
+    instance.save()
