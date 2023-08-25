3443cf9101bd5b476c19006ed6e905cac366ca16
Moin Abbas <moin.malik996@gmail.com>
2023-08-25 11:30:05 +0500

    debugging

--- Appointment/views/v1/views.py
+++ Appointment/views/v1/views.py
@@ -284,6 +284,7 @@ def get_employee_appointment_insights(request):
     formatted_date = start_date.strftime("%Y-%m-%d")
 
     if type(employee_ids) == str:
+        employee_ids = employee_ids.replace("'", '"')
         employee_ids = json.loads(employee_ids)
         data_type = "STR"
     elif type(employee_ids) == list:
