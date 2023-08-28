e078c6ad1e7db6927ee8b95ba199c3a022a888c1
abdullahaslam0079 <abdullah.aslam15327@gmail.com>
2023-08-28 23:18:08 +0500

    s3 bucket url updated

--- Product/Constants/index.py
+++ Product/Constants/index.py
@@ -4,7 +4,7 @@ from django.conf import settings
 
 def tenant_media_base_url(request, is_s3_url=False):
     if is_s3_url:
-        return f'{settings.CLOUD_FRONT_S3_BUCKET_URL}/media/{request.tenant_schema_name}/'
+        return f'{settings.CLOUD_FRONT_S3_BUCKET_URL}/{request.tenant_schema_name}/'
     else:
         return f'{settings.BACKEND_HOST}/media/{request.tenant_schema_name}/'
 
