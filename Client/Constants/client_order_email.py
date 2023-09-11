150f51336e179c129aa5eb6dd4b794bf357f465b
Moin Abbas <moin.malik996@gmail.com>
2023-09-11 14:54:56 +0500

    debugging

--- Client/Constants/client_order_email.py
+++ Client/Constants/client_order_email.py
@@ -14,7 +14,7 @@ def send_order_email(checkout, request):
     
     with tenant_context(request.tenant):
 
-        order_data = dict(SaleOrders_CheckoutSerializer(checkout_order, context={'request':request}).data)
+        order_data = dict(SaleOrders_CheckoutSerializer(checkout, context={'request':request}).data)
         
         #Summary
         total_tax = order_data['gst'] + order_data['gst1']
