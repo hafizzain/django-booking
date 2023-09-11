0ffc68a2762e1a87f1776f9b56dbd336c994953e
Moin Abbas <moin.malik996@gmail.com>
2023-09-11 17:31:23 +0500

    spelling mistakes

--- Client/Constants/client_order_email.py
+++ Client/Constants/client_order_email.py
@@ -73,7 +73,7 @@ def send_membership_order_email(membership_order, business_address, request):
         membership_currency = membership_order.membersheip.membership_currenypricemembership.filter(currency=business_address.currency).first()
         html_file = render_to_string('ClientOrderMail/membership_sale_order.html',
                                      {'membership_order':membership_order,
-                                      'membership_currency':membership_currencys}
+                                      'membership_currency':membership_currency}
                                     )
         text_content = strip_tags(html_file)
 
