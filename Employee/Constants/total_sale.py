4a413e0174781d8ee390d853a3c06be6cb7645eb
Moin Abbas <moin.malik996@gmail.com>
2023-09-21 13:27:27 +0500

    trying to fix total_SALE

--- Employee/Constants/total_sale.py
+++ Employee/Constants/total_sale.py
@@ -12,23 +12,23 @@ def total_sale_employee(employee_id):
     appointment_checkout = AppointmentService.objects.filter(
         appointment_status = 'Done',
         member = employee_id,
-        ).aggregate(appointment_sum=Sum('total_price', output_field=FloatField()))['appointment_sum']
+        ).aggregate(appointment_sum=Sum('total_price'))['appointment_sum']
     
     if appointment_checkout:
-        total_price += appointment_checkout
+        total_price += float(appointment_checkout)
 
 
     # Discounted Orders Sale------------------
     discounted_orders = Order.objects.filter(
         checkout__is_deleted = False, 
         member__id = employee_id,
-        discount_price__isnull=False
+        discount_price__isnull=False,
     ).annotate(
         total = F('discount_price') * F('quantity')
-    ).aggregate(total_sum=Sum('total', output_field=FloatField()))['total_sum']
+    ).aggregate(total_sum=Sum('total'))['total_sum']
 
     if discounted_orders:
-        total_price += discounted_orders
+        total_price += float(discounted_orders)
 
 
 
@@ -39,18 +39,8 @@ def total_sale_employee(employee_id):
         discount_price__isnull=True
     ).annotate(
         total = F('total_price') * F('quantity')
-    ).aggregate(total_sum=Sum('total',  output_field=FloatField()))['total_sum']
+    ).aggregate(total_sum=Sum('total'))['total_sum']
 
     if non_discounted_orders:
-        total_price += non_discounted_orders
+        total_price += float(non_discounted_orders)
 
-    # for order in orders:
-    #     price = 0
-    #     if order.discount_price:
-    #         price = order.discount_price
-    #     elif order.total_price:
-    #         price = order.total_price
-    #     total_price += float(price) * float(order.quantity)
-
-        
-    # return total_price
