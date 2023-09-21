a965f5e66656fcd393fa81f22b6995e9dc335182
Moin Abbas <moin.malik996@gmail.com>
2023-09-21 12:52:33 +0500

    fixed an d optimized some issues

--- Employee/Constants/total_sale.py
+++ Employee/Constants/total_sale.py
@@ -8,27 +8,49 @@ def total_sale_employee(employee_id):
     employee_id = str(employee_id)
     
 
-    # Appointments Sale----------------------------
+    # Appointments Sale-------------------------------------
     appointment_checkout = AppointmentService.objects.filter(
         appointment_status = 'Done',
         member = employee_id,
         ).aggregate(appointment_sum=Sum('total_price'))['appointment_sum']
     
-    total_price += appointment_checkout
+    if appointment_checkout:
+        total_price += appointment_checkout
 
 
-    # Orders Sale------------------
-    orders = Order.objects.filter(
+    # Discounted Orders Sale------------------
+    discounted_orders = Order.objects.filter(
         checkout__is_deleted = False, 
         member__id = employee_id,
-    )
-    for order in orders:
-        price = 0
-        if order.discount_price:
-            price = order.discount_price
-        elif order.total_price:
-            price = order.total_price
-        total_price += float(price) * float(order.quantity)
+        discount_price__isnull=False
+    ).annotate(
+        total = F('discount_price') * F('quantity')
+    ).aggregate(total_sum=Sum('total'))['total_sum']
+
+    if discounted_orders:
+        total_price += discounted_orders
+
+
+
+    # Non Discounted Sale-----------------------
+    non_discounted_orders = Order.objects.filter(
+        checkout__is_deleted = False, 
+        member__id = employee_id,
+        discount_price__isnull=True
+    ).annotate(
+        total = F('total_price') * F('quantity')
+    ).aggregate(total_sum=Sum('total'))['total_sum']
+
+    if non_discounted_orders:
+        total_price += non_discounted_orders
+
+    # for order in orders:
+    #     price = 0
+    #     if order.discount_price:
+    #         price = order.discount_price
+    #     elif order.total_price:
+    #         price = order.total_price
+    #     total_price += float(price) * float(order.quantity)
 
         
-    return total_price
+    # return total_price
