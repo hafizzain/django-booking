051345bb4c76781bbf24eeea708b98573d432380
HuzaifaYaqoob <zeevhyorg@gmail.com>
2023-10-04 11:23:26 +0500

    exception page pagination done

--- SuperInsight/views.py
+++ SuperInsight/views.py
@@ -16,6 +16,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from Authentication.models import User
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 status_codes = [
     100, 101, 200, 201, 202, 203, 204, 205, 206, 207, 208, 226, 300, 301, 302, 303, 304, 305, 306, 307, 308, 400, 401, 402, 403, 404, 405, 406, 407, 408, 409, 410, 411, 412, 413, 414, 415, 416, 417, 418, 422, 423, 424, 426, 428, 429, 431, 451, 500, 501, 502, 503, 504, 505, 506, 507, 508, 509, 510, 511,
@@ -85,6 +86,7 @@ def ExceptionPage(request):
     business_name = request.GET.get('business_name', None)
     request_method = request.GET.get('request_method', None)
     selected_date = request.GET.get('date', None)
+    page_ = request.GET.get('page', 1)
 
 
     query = {}
@@ -99,10 +101,24 @@ def ExceptionPage(request):
     
     if selected_date:
         query['created_at__date'] = selected_date
+    
 
-    exceptions = ExceptionRecord.objects.filter(**query).order_by('-created_at')
+    all_exceptions = ExceptionRecord.objects.filter(**query).order_by('-created_at')
+    paginator = Paginator(all_exceptions, 100)
+    try:
+        page_obj = paginator.page(int(page_))
+    except PageNotAnInteger:
+        page_obj = paginator.page(1)
+    except EmptyPage:
+        page_obj = paginator.page(paginator.num_pages)
+
+    print(len(page_obj))
+
     context={}
-    context['exceptions'] = exceptions
+    context['exceptions'] = page_obj
+    context['total_pages'] = int((all_exceptions.count() / 100)) + (1 if (all_exceptions.count() % 100) > 0 else 0 )
+    context['selected_page'] = page_
+    context['page_obj'] = page_obj
     context['status_codes'] = status_codes
     return render(request, 'SuperAdminPanel/pages/Exception/exception.html', context)
 
