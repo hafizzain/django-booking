2b162b81d8565bb12232dd7dcd90b096438d15c0
HuzaifaYaqoob <zeevhyorg@gmail.com>
2023-10-04 12:42:08 +0500

    tenant sale services

--- SuperInsight/views.py
+++ SuperInsight/views.py
@@ -17,6 +17,10 @@ from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from Authentication.models import User
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import Count, F
+
+from Appointment.models import AppointmentService
+from Service.models import Service
 
 status_codes = [
     100, 101, 200, 201, 202, 203, 204, 205, 206, 207, 208, 226, 300, 301, 302, 303, 304, 305, 306, 307, 308, 400, 401, 402, 403, 404, 405, 406, 407, 408, 409, 410, 411, 412, 413, 414, 415, 416, 417, 418, 422, 423, 424, 426, 428, 429, 431, 451, 500, 501, 502, 503, 504, 505, 506, 507, 508, 509, 510, 511,
@@ -40,9 +44,10 @@ def DashboardPage(request):
                 is_blocked = False,
             )
             clients += tenant_clients.count()
+            
     context = {
         'total_clients' : clients,
-        'get_country_users_url' : reverse('GetCountryClients')
+        'get_country_users_url' : reverse('GetCountryClients'),
     }
     return render(request, 'SuperAdminPanel/pages/dashboard/dashboard.html', context)
 
@@ -78,6 +83,39 @@ def GetCountryClients(request):
     return Response({
         'country_labels' : country_labels,
         'country_values' : country_values,
+    })
+
+@api_view(['GET',])
+@permission_classes([AllowAny])
+def GetTotalSaleCount(request):
+    tenants = Tenant.objects.filter(
+        is_active = True,
+        is_ready = True,
+        is_blocked = False,
+        is_deleted = False,
+    )
+    tenants_services = []
+    for tenant in tenants:
+        with tenant_context(tenant):
+            services = Service.objects.filter(
+                is_deleted = False,
+                is_active = True,
+                is_blocked = False,
+            ).annotate(
+                total_count = Count(F('serivce_appointments'))
+            ).values_list('name', flat=True)
+            tenants_services.extend(list(services))
+
+    
+    services_labels = set(tenants_services)
+    services_values = []
+
+    for s_name in tenants_services:
+        services_values.append(tenants_services.count(s_name))
+        
+    return Response({
+        'services_labels' : services_labels,
+        'services_values' : services_values,
     })
 
 @login_required(login_url='/super-admin/super-login/')
