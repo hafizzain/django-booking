d65ccd1b3b28c969450c82cc77e24916d42154d2
Moin Abbas <moin.malik996@gmail.com>
2023-10-12 11:45:22 +0500

    updated update location api

--- Business/serializers/v1_serializers.py
+++ Business/serializers/v1_serializers.py
@@ -383,6 +383,8 @@ class BusinessAddress_GetSerializer(serializers.ModelSerializer):
             'is_deleted',
             'is_publish',
             'description',
+            'primary_translation',
+            'secondary_translation'
         ]
 class BusinessAddress_CustomerSerializer(serializers.ModelSerializer):
     #opening_hours= OpeningHoursSerializer(read_only=True)
