261aade27c770d9febfff512c6e50e6c16b56445
Moin Abbas <moin.malik996@gmail.com>
2023-10-13 17:46:06 +0500

    product translation implemented

--- Invoices/templatetags/custom_tags.py
+++ Invoices/templatetags/custom_tags.py
@@ -1,16 +1,23 @@
 from django import template
 register = template.Library()
 
-from Order.models import ServiceOrder
+from Order.models import ServiceOrder, ProductOrder
 from MultiLanguage.models import InvoiceTranslation
 
 
 @register.simple_tag
 def get_translation(order_id):
     service_order = ServiceOrder.objects.filter(id=order_id).first()
+    product_order = ProductOrder.objects.filter(id=order_id).first()
+
     if service_order and service_order.location.secondary_translation:
         secondary_invoice_traslation = InvoiceTranslation.objects.filter(id=service_order.location.secondary_translation.id).first()
         translation = service_order.service.servicetranlations_set.get(language__id=secondary_invoice_traslation.language.id)
         return translation.service_name
-    else:
-        return ''
+    
+    if product_order and product_order.location.secondary_translation:
+        secondary_invoice_traslation = InvoiceTranslation.objects.filter(id=product_order.location.secondary_translation.id).first()
+        translation = product_order.product.producttranslations_set.get(language__id=secondary_invoice_traslation.language.id)
+        return translation.service_name
+    
+    return ''
