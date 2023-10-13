237c4ae85e00594cd993ee3db5908843d2f1c2b1
Moin Abbas <moin.malik996@gmail.com>
2023-10-13 16:55:43 +0500

    issue resolved

--- Invoices/templatetags/custom_tags.py
+++ Invoices/templatetags/custom_tags.py
@@ -7,7 +7,7 @@ from MultiLanguage.models import InvoiceTranslation
 
 @register.simple_tag
 def get_translation(order_id):
-    service_order = ServiceOrder.objects.filter(id=id).first()
+    service_order = ServiceOrder.objects.filter(id=order_id).first()
     if service_order and service_order.location.secondary_translation:
         secondary_invoice_traslation = InvoiceTranslation.objects.filter(id=service_order.location.secondary_translation.id).first()
         translation = service_order.service.servicetranlations_set.get(language__id=secondary_invoice_traslation.language.id)
