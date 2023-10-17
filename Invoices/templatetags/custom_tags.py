903c60b8d89bb7ae90250b320773491c57334b8c
Moin Abbas <moin.malik996@gmail.com>
2023-10-17 11:36:54 +0500

    fixed issue whilee getting data

--- Invoices/templatetags/custom_tags.py
+++ Invoices/templatetags/custom_tags.py
@@ -12,12 +12,12 @@ def get_translation(order_id):
 
     if service_order and service_order.location.secondary_translation:
         secondary_invoice_traslation = InvoiceTranslation.objects.filter(id=service_order.location.secondary_translation.id).first()
-        translation = service_order.service.servicetranlations_set.get(language__id=secondary_invoice_traslation.language.id)
-        return translation.service_name
+        translation = service_order.service.servicetranlations_set.filter(language__id=secondary_invoice_traslation.language.id).first()
+        return translation.service_name if translation else ''
     
     if product_order and product_order.location.secondary_translation:
         secondary_invoice_traslation = InvoiceTranslation.objects.filter(id=product_order.location.secondary_translation.id).first()
-        translation = product_order.product.producttranslations_set.get(language__id=secondary_invoice_traslation.language.id)
-        return translation.product_name
+        translation = product_order.product.producttranslations_set.filter(language__id=secondary_invoice_traslation.language.id).first()
+        return translation.product_name if translation else ''
     
     return ''
