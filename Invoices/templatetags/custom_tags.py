33017e41b2a4c351e3b18a6a637962abfa552d1d
Moin Abbas <moin.malik996@gmail.com>
2023-10-17 15:58:04 +0500

    product add login there

--- Invoices/templatetags/custom_tags.py
+++ Invoices/templatetags/custom_tags.py
@@ -28,13 +28,13 @@ def get_primary_translation(order_id):
     product_order = ProductOrder.objects.filter(id=order_id).first()
 
     if service_order and service_order.location.primary_translation:
-        secondary_invoice_traslation = InvoiceTranslation.objects.filter(id=service_order.location.primary_translation.id).first()
-        translation = service_order.service.servicetranlations_set.filter(language__id=secondary_invoice_traslation.language.id).first()
+        primary_invoice_traslation = InvoiceTranslation.objects.filter(id=service_order.location.primary_translation.id).first()
+        translation = service_order.service.servicetranlations_set.filter(language__id=primary_invoice_traslation.language.id).first()
         return translation.service_name if translation else ''
     
     if product_order and product_order.location.primary_translation:
-        secondary_invoice_traslation = InvoiceTranslation.objects.filter(id=product_order.location.primary_translation.id).first()
-        translation = product_order.product.producttranslations_set.filter(language__id=secondary_invoice_traslation.language.id).first()
+        primary_invoice_traslation = InvoiceTranslation.objects.filter(id=product_order.location.primary_translation.id).first()
+        translation = product_order.product.producttranslations_set.filter(language__id=primary_invoice_traslation.language.id).first()
         return translation.product_name if translation else ''
     
     return ''
