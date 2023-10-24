2e5ddca76a0bfe70c174124723c844ee9c917879
Moin Abbas <moin.malik996@gmail.com>
2023-10-24 16:02:17 +0500

    fixed the issue

--- Client/views/v1/views.py
+++ Client/views/v1/views.py
@@ -171,18 +171,18 @@ def get_single_client(request):
 def get_client(request):
     no_pagination = request.GET.get('no_pagination', None)
     search_text = request.GET.get('search_text', None)
-    client_status = request.GET.get('status', None)
+    is_active = request.GET.get('active', None)
     all_client = Client.objects.filter(is_deleted=False, is_blocked=False).order_by('-created_at').distinct()
 
     if search_text:
         all_client = all_client.filter(full_name__icontains=search_text)
 
-    if client_status is not None:
+    if is_active is not None:
 
-        if client_status == True:
+        if is_active == True:
             all_client = all_client.filter(is_active=True)
         
-        if client_status == False:
+        if is_active == False:
             all_client = all_client.filter(is_active=False)
 
     all_client_count=all_client.count()
