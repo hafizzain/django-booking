2a7477ce22c91572092621fc5d12199006dd9ab6
Moin Abbas <moin.malik996@gmail.com>
2023-10-24 17:22:50 +0500

    removed debugging command

--- Client/views/v1/views.py
+++ Client/views/v1/views.py
@@ -173,21 +173,16 @@ def get_client(request):
     search_text = request.GET.get('search_text', None)
     is_active = request.GET.get('active', None)
     all_client = Client.objects.filter(is_deleted=False, is_blocked=False).order_by('-created_at').distinct()
-    coming_outer = None
-    value = None
 
     if search_text:
         all_client = all_client.filter(full_name__icontains=search_text)
 
     if is_active is not None:
-        coming_outer = 'True'
         if is_active == 'true':
             all_client = all_client.filter(is_active=True)
-            value = 'True'
         
         if is_active == 'false':
             all_client = all_client.filter(is_active=False)
-            value = 'False'
 
     all_client_count=all_client.count()
 
@@ -211,8 +206,6 @@ def get_client(request):
                 'per_page_result':results_per_page,
                 'error_message' : None,
                 'client' : serialized.data,
-                'not_none': coming_outer,
-                'value': value,
             }
         },
         status=status.HTTP_200_OK
