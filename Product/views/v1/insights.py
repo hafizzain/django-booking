cca3fde959febe8acf36e2383d9f071f7afc67a1
Moin Abbas <moin.malik996@gmail.com>
2023-10-25 13:12:48 +0500

    reformatting the code

--- Product/views/v1/insights.py
+++ Product/views/v1/insights.py
@@ -112,9 +112,10 @@ class FilteredInsightProducts(APIView):
                 if value in ['LAST_7_DAYS', 'LAST_30_DAYS', 'MOST_ORDERED_PRODUCTS']:
                     value = MOST_ORDERED_CHOICES.get(value)
                     self.queries['filter']['product_order_stock__order__to_location__id'] = self.location
+
         elif self.is_date_most_ordered and self.start_date and self.end_date:
-            
             self.queries['filter']['product_order_stock__order__created_at__range'] = (self.start_date, self.end_date)
+            
         else:
             return Response(
                 {
