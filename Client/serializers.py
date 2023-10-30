f8a8f416a9c2c2d58d3238e920d8e3f5c8183fde
Moin Abbas <moin.malik996@gmail.com>
2023-10-30 15:37:58 +0500

    out of range resolve

--- Client/serializers.py
+++ Client/serializers.py
@@ -148,7 +148,7 @@ class ClientSerializer(serializers.ModelSerializer):
     def get_last_transaction(self, obj):
         data = list(self.last_sale) + list(self.last_appointment)
         data = sorted(data, key=lambda x:x['created_at'], reverse=True)
-        return data[0]
+        return data
 
     def get_total_done_appointments(self, obj):
         return AppointmentService.objects.filter(
