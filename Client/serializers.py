714aa7c938cb054796a6eb8ac21a3d35fbe23075
Moin Abbas <moin.malik996@gmail.com>
2023-10-30 16:28:19 +0500

    list index out of range

--- Client/serializers.py
+++ Client/serializers.py
@@ -148,7 +148,7 @@ class ClientSerializer(serializers.ModelSerializer):
     def get_last_transaction(self, obj):
         data = self.last_sale + self.last_appointment
         sorted_data = sorted(data, key=lambda x: x['created_at'])
-        return sorted_data[1]
+        return sorted_data
 
     def get_total_done_appointments(self, obj):
         return AppointmentService.objects.filter(
