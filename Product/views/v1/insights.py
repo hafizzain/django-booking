9e78ac9578c178dc45d224dade0fdaf5ac3b4b8e
Moin Abbas <moin.malik996@gmail.com>
2023-11-06 15:12:20 +0500

    more changes to bound them

--- Product/views/v1/insights.py
+++ Product/views/v1/insights.py
@@ -302,32 +302,36 @@ class FilteredInsightProducts(APIView):
             }
 
             # Told Sold
-            if (self.top_sold or self.is_date_most_ordered) and (product_instance.top_sold_orders or product_instance.top_sold_orders == 0):
-                product['top_sold_orders'] = int(product_instance.top_sold_orders)
-                product['quantity'] = int(product_instance.top_sold_orders)
-            else:
-                product['top_sold_orders'] = self.top_sold
+            if top_sold or is_date_top_sold:
+                if (self.top_sold or self.is_date_most_ordered) and (product_instance.top_sold_orders or product_instance.top_sold_orders == 0):
+                    product['top_sold_orders'] = int(product_instance.top_sold_orders)
+                    product['quantity'] = int(product_instance.top_sold_orders)
+                else:
+                    product['top_sold_orders'] = self.top_sold
 
             # Most Consumed
-            if (self.most_consumed or self.is_date_most_ordered) and (product_instance.most_consumed_products or product_instance.most_consumed_products == 0):
-                product['most_consumed_products'] = int(product_instance.most_consumed_products)
-                product['quantity'] = int(product_instance.most_consumed_products)
-            else:
-                product['most_consumed_products'] = self.most_consumed
+            if most_consumed or is_date_most_consumed:
+                if (self.most_consumed or self.is_date_most_ordered) and (product_instance.most_consumed_products or product_instance.most_consumed_products == 0):
+                    product['most_consumed_products'] = int(product_instance.most_consumed_products)
+                    product['quantity'] = int(product_instance.most_consumed_products)
+                else:
+                    product['most_consumed_products'] = self.most_consumed
 
             # Most Ordered
-            if (self.most_ordered or self.is_date_most_ordered) and (product_instance.most_ordered_products or product_instance.most_ordered_products == 0):
-                product['most_ordered_products'] = int(product_instance.most_ordered_products)
-                product['quantity'] = int(product_instance.most_ordered_products)
-            else:
-                product['most_ordered_products'] = self.most_ordered or self.is_date_most_ordered
+            if most_ordered or is_date_most_ordered:
+                if (self.most_ordered or self.is_date_most_ordered) and (product_instance.most_ordered_products or product_instance.most_ordered_products == 0):
+                    product['most_ordered_products'] = int(product_instance.most_ordered_products)
+                    product['quantity'] = int(product_instance.most_ordered_products)
+                else:
+                    product['most_ordered_products'] = self.most_ordered or self.is_date_most_ordered
             
             # Most Transfered
-            if (self.most_transferred or self.is_date_most_ordered) and (product_instance.most_transferred_products or product_instance.most_transferred_products == 0):
-                product['most_transferred_products'] = int(product_instance.most_transferred_products)
-                product['quantity'] = int(product_instance.most_transferred_products)
-            else:
-                product['most_transferred_products'] = self.most_transferred
+            if most_transferred or is_date_most_transferred:
+                if (self.most_transferred or self.is_date_most_ordered) and (product_instance.most_transferred_products or product_instance.most_transferred_products == 0):
+                    product['most_transferred_products'] = int(product_instance.most_transferred_products)
+                    product['quantity'] = int(product_instance.most_transferred_products)
+                else:
+                    product['most_transferred_products'] = self.most_transferred
 
 
             # if self.low_stock_products :
