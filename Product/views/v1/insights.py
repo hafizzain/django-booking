839ec5eb8490e409724c16704a0ebf5c5d819c5c
Moin Abbas <moin.malik996@gmail.com>
2023-11-06 15:57:58 +0500

    debugging

--- Product/views/v1/insights.py
+++ Product/views/v1/insights.py
@@ -155,7 +155,7 @@ class FilteredInsightProducts(APIView):
         self.end_date = request.GET.get('endDate', None)
 
         self.queries['order_by'].append('-most_transferred_products')
-        self.queries['annotate']['most_transferred_products'] = Sum('products_stock_transfers__quantity')
+        self.queries['annotate']['most_transferred_products'] = Sum('products_stock_transfers__quantity', distinct=True)
         self.queries['filter']['products_stock_transfers__from_location__id'] = self.location
 
 
