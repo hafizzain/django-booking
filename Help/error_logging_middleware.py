557258b6b2f8d4ab062c61aa8fee7e699367c076
Moin Abbas <moin.malik996@gmail.com>
2023-11-07 11:36:52 +0500

    reverting the changes

--- Help/error_logging_middleware.py
+++ Help/error_logging_middleware.py
@@ -2,17 +2,12 @@ from Utility.models import ExceptionRecord
 import logging
 from Tenants.models import Tenant
 from django_tenants.utils import tenant_context
-from django.db import connection, transaction
+from django.db import connection
 class ServerErrorLoggingMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        if request.method in ["PUT", "POST"]:
-            with transaction.atomic():
-                response = self.get_response(request)
-        else:
-            response = self.get_response(request)
         response = self.get_response(request)
         if 500 <= response.status_code < 600:
             # Log the server error
