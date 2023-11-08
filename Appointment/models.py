8eb323d3b3d414ac5ce7e562f0809374bad9b348
Moin Abbas <moin.malik996@gmail.com>
2023-11-08 14:42:24 +0500

    fixed some more bugs

--- Appointment/models.py
+++ Appointment/models.py
@@ -319,7 +319,7 @@ class AppointmentCheckout(models.Model):
 
     def get_client_loyalty_points(self):
         if self.client:
-            redeemed_points_obj = LoyaltyPointLogs.objects.filter(client=self.client,
+            redeemed_points_obj = LoyaltyPointLogs.objects.filter(client=self.appointment.client,
                                                               location=self.business_address) \
                                                         .order_by('-created_at')
             if redeemed_points_obj:
