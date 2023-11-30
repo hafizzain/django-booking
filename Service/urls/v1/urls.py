e2836f114a99685b167e7d4400939cc4190255eb
Moin Abbas <moin.malik996@gmail.com>
2023-11-30 17:30:57 +0500

    services added

--- Service/urls/v1/urls.py
+++ Service/urls/v1/urls.py
@@ -5,5 +5,7 @@ from Service.views.v1 import views
 urlpatterns = [
     #Employee
     path('get_services/', views.get_services),
+    path('get_services_dropdown/', views.get_services_dropdown),
+
 
 ]
