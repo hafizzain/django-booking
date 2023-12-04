753edef424d265dbf1f8d9919e84f3902a9e5a4d
Moin Abbas <moin.malik996@gmail.com>
2023-12-04 17:18:02 +0500

    debugging

--- Product/serializers.py
+++ Product/serializers.py
@@ -349,7 +349,8 @@ class ProductWithStockSerializerOP(serializers.ModelSerializer):
 
     class Meta:
         model = Product
-        fields = ['id', 'name', 'stock','currency_retail_price', 'total_transfer', 'total_consumption']
+        fields = ['id', 'name', 'stock','currency_retail_price', 'total_transfer', 'total_consumption',
+                  'total_transfer_debug']
         read_only_fields = ['id']
         
 
