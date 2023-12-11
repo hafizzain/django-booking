893f5549596e64a5268407240544e23b37ecacd3
Moin Abbas <moin.malik996@gmail.com>
2023-12-11 15:22:22 +0500

    again a migration was required

--- Appointment/models.py
+++ Appointment/models.py
@@ -179,6 +179,8 @@ class AppointmentService(models.Model):
     discount_percentage = models.FloatField(default = 0 , null=True, blank=True)
 
     total_price = models.FloatField(default = 0 , null=True, blank=True)
+    service_start_time = models.DateTimeField(default=None)
+    service_end_time = models.DateTimeField(default=None)
 
     # redeemed attributes
     is_redeemed = models.BooleanField(default=False)
