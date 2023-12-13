7dbd7fc23e8328ff8d6ed74268d4a35d244783b5
Moin Abbas <moin.malik996@gmail.com>
2023-12-13 17:17:41 +0500

    some debugging

--- NStyle/settings.py
+++ NStyle/settings.py
@@ -248,13 +248,13 @@ REST_FRAMEWORK = {
 
     'DEFAULT_RENDERER_CLASSES': DEFAULT_RENDERER_CLASSES,
 
-    'DEFAULT_AUTHENTICATION_CLASSES' : [
-        'rest_framework.authentication.TokenAuthentication'
-    ],
+    # 'DEFAULT_AUTHENTICATION_CLASSES' : [
+    #     'rest_framework.authentication.TokenAuthentication'
+    # ],
 
-    'DEFAULT_PERMISSION_CLASSES' : [
-        'rest_framework.permissions.IsAuthenticated'
-    ],
+    # 'DEFAULT_PERMISSION_CLASSES' : [
+    #     'rest_framework.permissions.IsAuthenticated'
+    # ],
 
 }
 
