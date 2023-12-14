3fd4ff25965126652d26dac693e639ef80839fd0
MArbabsabir336 <Arbabsabir336@gmail.com>
2023-12-14 14:40:40 +0500

    resolve unexpected keyword argument id and has no attribute

--- CRM/serializers.py
+++ CRM/serializers.py
@@ -1,8 +1,6 @@
 from rest_framework import serializers
 from Product.Constants.index import tenant_media_base_url
 from CRM.models import *
-from Appointment.models import AppointmentService
-from Order.models import Order, Checkout
 from Appointment.models import Appointment
 
 class SegmentSerializer(serializers.ModelSerializer):
