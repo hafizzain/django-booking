d36c62be1249191790ba7c918a5bbd289fbc8eee
MArbabsabir336 <Arbabsabir336@gmail.com>
2023-12-14 14:52:37 +0500

    change id to pk in urls

--- CRM/urls/v1/urls.py
+++ CRM/urls/v1/urls.py
@@ -4,15 +4,15 @@ from Client.views.v1.views import get_client_dropdown
 
 urlpatterns = [
     path('segment-list/', SegmentAPIView.as_view()),
-    path('segment/<str:id>/', SegmentAPIView.as_view()),
+    path('segment/<str:pk>/', SegmentAPIView.as_view()),
     path('segment-create', SegmentAPIView.as_view()),
-    path('segment-update/<str:id>/', SegmentAPIView.as_view()),
-    path('segment-delete/<str:id>/', SegmentAPIView.as_view()),
+    path('segment-update/<str:pk>/', SegmentAPIView.as_view()),
+    path('segment-delete/<str:pk>/', SegmentAPIView.as_view()),
     path('campaign-list/', CampaignsAPIView.as_view()),
-    path('campaign/<str:id>/', CampaignsAPIView.as_view()),
+    path('campaign/<str:pk>/', CampaignsAPIView.as_view()),
     path('campaign-create', CampaignsAPIView.as_view()),
-    path('campaign-update/<str:id>/', CampaignsAPIView.as_view()),
-    path('campaign-delete/<str:id>/', CampaignsAPIView.as_view()),
+    path('campaign-update/<str:pk>/', CampaignsAPIView.as_view()),
+    path('campaign-delete/<str:pk>/', CampaignsAPIView.as_view()),
     path('client-filter/', get_client_dropdown)
    
 ]
