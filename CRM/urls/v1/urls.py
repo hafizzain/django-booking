8ad44c02e6bff4944e2f1cdbc884141de0738b66
MArbabsabir336 <Arbabsabir336@gmail.com>
2023-12-14 14:56:42 +0500

    all  urls end with slash

--- CRM/urls/v1/urls.py
+++ CRM/urls/v1/urls.py
@@ -5,12 +5,12 @@ from Client.views.v1.views import get_client_dropdown
 urlpatterns = [
     path('segment-list/', SegmentAPIView.as_view()),
     path('segment/<str:pk>/', SegmentAPIView.as_view()),
-    path('segment-create', SegmentAPIView.as_view()),
+    path('segment-create/', SegmentAPIView.as_view()),
     path('segment-update/<str:pk>/', SegmentAPIView.as_view()),
     path('segment-delete/<str:pk>/', SegmentAPIView.as_view()),
     path('campaign-list/', CampaignsAPIView.as_view()),
     path('campaign/<str:pk>/', CampaignsAPIView.as_view()),
-    path('campaign-create', CampaignsAPIView.as_view()),
+    path('campaign-create/', CampaignsAPIView.as_view()),
     path('campaign-update/<str:pk>/', CampaignsAPIView.as_view()),
     path('campaign-delete/<str:pk>/', CampaignsAPIView.as_view()),
     path('client-filter/', get_client_dropdown)
