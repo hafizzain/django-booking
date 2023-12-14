88c0cde8f9581d1e86334261022b5ad9cb9f0228
MArbabsabir336 <Arbabsabir336@gmail.com>
2023-12-14 17:19:11 +0500

    change api response and get client name and image

--- CRM/serializers.py
+++ CRM/serializers.py
@@ -2,21 +2,26 @@ from rest_framework import serializers
 from Product.Constants.index import tenant_media_base_url
 from CRM.models import *
 from Appointment.models import Appointment
+from Client.models import Client
 
+
+class ClientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Client
+        fields = ['id', 'full_name', 'image'] 
+        
+        
 class SegmentSerializer(serializers.ModelSerializer):
+    clients = ClientSerializer(many=True, read_only=True, source='client')
     
     class Meta:
         model =  Segment
         fields = '__all__'
-        
-    # def create(self, validated_data):
-    #     user = self.context['request'].user
-    #     segment = Segment.objects.create(
-    #         **validated_data,
-    #         user=user
-    #     )
-        
-    #     return segment
+    
+    def get_client_data(self, obj):
+        return [{'full_name': client.full_name,
+                    'image': client.image} for client in obj.client.all()]
+
         
 class CampaignsSerializer(serializers.ModelSerializer):
         
