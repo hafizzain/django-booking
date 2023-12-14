d216b6561cd04c1a4fd53ffa276dcde845700287
Moin Abbas <moin.malik996@gmail.com>
2023-12-14 17:40:14 +0500

    delete api implemented

--- Appointment/views/v1/views.py
+++ Appointment/views/v1/views.py
@@ -3423,4 +3423,10 @@ class MissedOpportunityListCreate(generics.ListAPIView):
                 'missed_opportunities' : data
             }
         }, status=status.HTTP_200_OK)
+    
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
