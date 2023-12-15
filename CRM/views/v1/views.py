85ef1a93fbe9ff756ba02beb502f0706f28a7795
MArbabsabir336 <Arbabsabir336@gmail.com>
2023-12-15 16:03:10 +0500

    revert name errer code

--- CRM/views/v1/views.py
+++ CRM/views/v1/views.py
@@ -111,32 +111,6 @@ class SegmentAPIView(APIView):
         serializer = SegmentSerializer(data=request.data,
                                        context={'request': request})
         
-        try:
-            serializer.is_valid(raise_exception=True)
-        except serializers.ValidationError as e:
-            if "name" in e.detail and "unique" in e.detail["name"][0].lower():
-                    data = {
-                        "success": False,
-                        "status_code": 302,
-                        "response": {
-                            "message": "Segment not created",
-                            "error_message": "Segment with this name already exists.",
-                            "data": None
-                        }
-                    }
-                    return Response(data, status=status.HTTP_302_FOUND)
-            else:
-                data = {
-                    "success": False,
-                    "status_code" : 400,
-                    "response" : {
-                        "message" : "Segment not created",
-                        "error_message" : serializer.errors,
-                        "data" : None
-                    }
-                }
-                return Response(data, status=status.HTTP_400_BAD_REQUEST) 
-
         if serializer.is_valid():
             serializer.save()
             data = {
@@ -149,7 +123,18 @@ class SegmentAPIView(APIView):
                     }
                 }
             return Response(data, status=status.HTTP_200_OK)
-         
+        else:   
+            data = {
+                    "success": False,
+                    "status_code" : 400,
+                    "response" : {
+                        "message" : "Segment not created",
+                        "error_message" : serializer.errors,
+                        "data" : None
+                    }
+                }
+            return Response(data, status=status.HTTP_400_BAD_REQUEST) 
+
     @transaction.atomic
     def put(self, request, pk):
         segment = get_object_or_404(Segment, id=pk)
