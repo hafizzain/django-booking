e27c8e50d2bf3adcb0b2b86d67afa63dbf13d88f
HuzaifaYaqoob <zeevhyorg@gmail.com>
2023-12-18 17:20:42 +0500

    is favourite added in create appointment

--- Appointment/serializers.py
+++ Appointment/serializers.py
@@ -758,7 +758,7 @@ class AllAppoinment_EmployeeSerializer(serializers.ModelSerializer):
                  'appointment_date', 'appointment_time', 'duration','srv_name','status',
                  'booked_by' , 'booking_id', 'appointment_type','client_can_book','slot_availible_for_online',
                  'appointment_status', 'location','employee_list', 'created_at', 'is_deleted', 'appointment_service_member',
-                 'service_start_time', 'service_end_time')
+                 'service_start_time', 'service_end_time', 'is_favourite',)
         
       
 class SingleAppointmentSerializer(serializers.ModelSerializer):
