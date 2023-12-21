d884968e67942cb6038440566b9c72d65233f624
hassan <hassanbutt0030@gmail.com>
2023-12-21 15:11:50 +0500

    views file updated

--- Appointment/views/v1/views.py
+++ Appointment/views/v1/views.py
@@ -3535,9 +3535,9 @@ def get_available_appointments(request):
             or_query = Q(client__full_name__icontains=search_text) | \
                        Q(client__full_name__icontains=search_text) | \
                        Q(user__full_name__icontains=search_text) | \
+                       Q(member__full_name__icontains=search_text) | \
                        Q(appointment_services__service__name__icontains=search_text) | \
                        Q(member__id__icontains=search_text) | \
-                       Q(member__full_name__icontains=search_text) | \
                        Q(client__id__icontains=search_text) | \
                        Q(appointment_services__id__icontains=search_text) | \
                        Q(id__icontains=search_text) | \
