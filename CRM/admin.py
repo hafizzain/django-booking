8b45f0c66624bf5c37ab790a1095800b2589a266
MArbabsabir336 <Arbabsabir336@gmail.com>
2023-12-22 12:04:28 +0500

    add model in admin site of CRM

--- CRM/admin.py
+++ CRM/admin.py
@@ -5,5 +5,14 @@ from django.contrib import admin
 from .models import Campaign, Segment
 
 
-admin.site.register(Campaign)
-admin.site.register(Segment)
+@admin.register(Campaign)
+class CampaignAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'content', 'start_date', 'end_date', 'campaign_type', 'segment', 'user']
+    search_fields = ['title', 'content']
+    ordering = ['id']
+    
+@admin.register(Segment)
+class SegmentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'segment_type', 'description', 'user', 'business']
+    search_fields = ['name', 'description']
+    ordering = ['id']
