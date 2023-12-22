19a2e4605a04ded8b10e7ccd5c9ea5a4c4e0fbd9
hassan <hassanbutt0030@gmail.com>
2023-12-22 15:08:20 +0500

    views file updated

--- Appointment/views/v1/views.py
+++ Appointment/views/v1/views.py
@@ -2554,9 +2554,12 @@ def get_client_sale(request):
 
     appointment = ServiceClientSaleSerializer(appointment_checkout_5[:5], many=True)
     quick_sale_count = len(product.data) + len(services_data.data)
-    for item in product:
-        print("i am printing item",item)
-    total_sale = total_sale
+    # for item in product:
+    #     print("i am printing item",item)
+    price_values = sum(item.get('price', 0) for item in product.data)
+    voucher_total_price = 0
+    voucher_total_price = sum(item.get('price', 0) for item in voucher.data)
+    total_sale = total_sale + price_values + voucher_total_price
     return Response(
         {
             'status': True,
