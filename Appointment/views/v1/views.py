5ffbb10c71396675bf3d80b331c67448b55484d2
Moin Abbas <moin.malik996@gmail.com>
2023-12-25 16:56:43 +0500

    checking the appointment status

--- Appointment/views/v1/views.py
+++ Appointment/views/v1/views.py
@@ -1826,8 +1826,6 @@ def create_checkout(request):
             },
             status=status.HTTP_404_NOT_FOUND
         )
-    appointment.status = 'Done'
-    appointment.save()
     if type(tip) == str:
         tip = json.loads(tip)
     if type(tip) == list:
