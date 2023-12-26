e71a21e588ff772e5b95ee30d7cc90451f30067a
M Asad <mughalasad720@gmail.com>
2023-12-26 11:53:58 +0500

    Resolve the error from the admin of RefundServices

--- Finance/admin.py
+++ Finance/admin.py
@@ -1,19 +1,14 @@
-# from django.contrib import admin
-# from .models import Refund, RefundProduct, Coupon
-# # Register your models here.
+from django.contrib import admin
+from Finance.models import Refund, RefundProduct, Coupon
 
-# @admin.register(Coupon)
-# class Coupon(admin.TabularInline):
-#     model = Coupon
-# @admin.register(RefundProduct)
-# class RefundProductsAdmin(admin.TabularInline):
-#     model = RefundProduct
+class CouponInline(admin.TabularInline):
+    model = Coupon
+
+class RefundProductsAdmin(admin.TabularInline):
+    model = RefundProduct
     
-# # class RefundServicesAdmin(RefundServices):
-# #     model = RefundServices
-
-# @admin.register(Refund)
-# class RefundAdmin(admin.ModelAdmin):
-#     inlines = [RefundProductsAdmin,Coupon]
-#     ordering = ['-created_at']
-#     list_display = ['id','user','refund_invoice_id','refund_type','total_refund_amount']
+@admin.register(Refund)
+class RefundAdmin(admin.ModelAdmin):
+    inlines = [RefundProductsAdmin, CouponInline]
+    ordering = ['-created_at']
+    list_display = ['id', 'user', 'refund_invoice_id', 'refund_type', 'total_refund_amount']
