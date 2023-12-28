3ad7786e15ae25d92bef09a65225d745d7f1e272
HuzaifaYaqoob <zeevhyorg@gmail.com>
2023-12-28 18:35:10 +0500

    Auto commit at 2023-12-28 18:35:10

--- Appointment/models.py
+++ Appointment/models.py
@@ -348,7 +348,7 @@ class AppointmentService(models.Model):
             
             last_month = int(datetime.now().strftime('%m'))
             months = max(last_month - client_f_month , 1)
-            
+
             if client_appointments.count() >= months:
                 tag = 'Most Visitor'
             else:
@@ -362,6 +362,13 @@ class AppointmentService(models.Model):
 
             client.client_tag = tag
             client.client_type = client_type
+        
+        if self.appointment.client:
+            if not self.client_tag:
+                self.client_tag = self.appointment.client.client_tag
+
+            if not self.client_type:
+                self.client_type = self.appointment.client.client_type
     
         super(AppointmentService, self).save(*args, **kwargs)
     
