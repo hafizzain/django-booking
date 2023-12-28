12af31764fc9ecf7f1c6a051faea1c30a21fe8b4
Moin Abbas <moin.malik996@gmail.com>
2023-12-28 16:10:52 +0500

    serializer also done

--- Service/serializers.py
+++ Service/serializers.py
@@ -15,12 +15,7 @@ class ServiceTranslationsSerializer(serializers.ModelSerializer):
 class BasicServiceSerializer(serializers.ModelSerializer):
     appointment_count = serializers.IntegerField()
     total_orders_quantity = serializers.IntegerField()
-    service_orders = serializers.SerializerMethodField()
-
-    def get_service_orders(self, obj):
-        return obj.service_orders.values()
-         
 
     class Meta:
         model = Service
-        fields = ['id', 'name', 'appointment_count', 'total_orders_quantity', 'service_orders']
+        fields = ['id', 'name', 'appointment_count', 'total_orders_quantity']
