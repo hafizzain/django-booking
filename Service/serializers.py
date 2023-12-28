8685997eeb7a242bc7ff45c31bca55ff940a9f03
Moin Abbas <moin.malik996@gmail.com>
2023-12-28 15:49:50 +0500

    some debugging

--- Service/serializers.py
+++ Service/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
 from Service.models import Service, ServiceTranlations
-
 class ServiceSerializer(serializers.ModelSerializer):
     
     class Meta:
@@ -16,6 +15,11 @@ class ServiceTranslationsSerializer(serializers.ModelSerializer):
 class BasicServiceSerializer(serializers.ModelSerializer):
     appointment_count = serializers.IntegerField()
     total_orders_quantity = serializers.IntegerField()
+    service_orders = serializers.SerializerMethodField()
+
+    def get_service_orders(self, obj):
+        return obj.service_orders.values()
+         
 
     class Meta:
         model = Service
