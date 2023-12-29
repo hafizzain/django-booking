dcaddc59e454909a081437b11f5dde3f762a4a38
HuzaifaYaqoob <zeevhyorg@gmail.com>
2023-12-29 11:52:53 +0500

    Auto commit at 2023-12-29 11:52:53

--- Appointment/signals.py
+++ Appointment/signals.py
@@ -4,11 +4,11 @@ from django.dispatch import receiver
 
 from django.db.models.signals import post_save
 
-from Appointment.models import AppointmentService
+# from Appointment.models import AppointmentService
 
-@receiver(post_save, sender=AppointmentService)
-def onAppointmentCreateAddClientStatus(sender, instance, created, **kwargs):
-    if created and instance.appointment and instance.appointment.client:
-        instance.client_tag = instance.appointment.client.client_tag
-        instance.client_type = instance.appointment.client.client_type
-        instance.save()
+# @receiver(post_save, sender=AppointmentService)
+# def onAppointmentCreateAddClientStatus(sender, instance, created, **kwargs):
+#     if created and instance.appointment and instance.appointment.client:
+#         instance.client_tag = instance.appointment.client.client_tag
+#         instance.client_type = instance.appointment.client.client_type
+#         instance.save()
