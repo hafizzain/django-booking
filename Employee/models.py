4a76f911d8e7ed583a16675202369b7b1128b95b
hassan <hassanbutt0030@gmail.com>
2023-12-29 16:38:57 +0500

    created_at added in holiday

--- Employee/models.py
+++ Employee/models.py
@@ -127,7 +127,7 @@ class Employee(models.Model):
     joining_date = models.DateField(null=True, blank=True)
     to_present = models.BooleanField(default=False)
     ending_date = models.DateField(null=True, blank=True)
-    can_refund = models.BooleanField(default=True)
+    # can_refund = models.BooleanField(default=True)
         
     is_default = models.BooleanField(default=False)
 
