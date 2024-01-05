e6adeb0e769629723d9c664e9fdc96895b76d3f3
MArbabsabir336 <Arbabsabir336@gmail.com>
2024-01-05 16:13:38 +0500

    add context request for image get in services

--- Service/views/v1/views.py
+++ Service/views/v1/views.py
@@ -27,7 +27,7 @@ def get_services(request):
     
     query = Q(is_deleted=False)
     service= Service.objects.filter(query).order_by('-created_at')
-    serialized = ServiceSerializer(service,  many=True, )
+    serialized = ServiceSerializer(service,  many=True, context={'request': request})
     return Response(
         {
             'status' : 200,
