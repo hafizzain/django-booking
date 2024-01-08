768bcf05e95c34781c17b9f22aef139f1b09555a
hassan <hassanbutt0030@gmail.com>
2024-01-08 16:33:43 +0500

    views updated

--- Employee/views/v1/views.py
+++ Employee/views/v1/views.py
@@ -3925,6 +3925,27 @@ def create_vacation_emp(request):
         value = employee_leave_management_obj.medical_leave
     if vacation_type == 'annual':
         value = employee_leave_management_obj.annual_leave
+        now = datetime.now()
+        employee_id = Employee.objects.get(id=employee, is_deleted=False)
+        created_at = employee_id.created_at
+        employee_leave_management_obj = LeaveManagements.objects.get(employee_id=employee)
+        required_months = employee_leave_management_obj.number_of_months
+        required_months = int(required_months)
+        months_difference = (now.year - created_at.year) * 12 + now.month - created_at.month
+        months_difference = int(months_difference)
+        if required_months > months_difference:
+            return Response(
+                {
+                    'status': 400,
+                    'status_code': '400',
+                    'response': {
+                        'message': 'Annual leave requests available after {required_months}'.format(
+                            required_months=required_months),
+                        'error_message': None,
+                    }
+                },
+                status=status.HTTP_200_OK
+            )
     if vacation_type == 'casual':
         value = employee_leave_management_obj.casual_leave
     from_date = datetime.strptime(from_date, "%Y-%m-%d")
