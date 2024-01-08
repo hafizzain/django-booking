db3e695ef43aad512e88f70a6470e83c6b031b08
M Asad <mughalasad720@gmail.com>
2024-01-08 12:56:43 +0500

    Added permission check

--- Finance/helpers.py
+++ Finance/helpers.py
@@ -1,11 +1,31 @@
 
 from django.db.models import Q
 from Finance.models import AllowRefundPermissionsEmployees
+from Invoices.models import SaleInvoice
+from django.utils import timezone
+
+
 # Short the UUID field Method
 def short_uuid(uuid):
     uuid = str(uuid).replace('-','')[:6]
     return uuid
 
-def check_permission(id):
-    AllowRefundPermissionsEmployees.objects.filter(Q(employee_id=id) & Q(allowed_refund__number_of_days__lt=30)).exists() or \
-        AllowRefundPermissionsEmployees.objects.filter(Q(employee_id=id) & Q(allowed_refund__number_of_days__gt=30)).exists()
+
+
+def check_days(invoice_id, location):
+    try:
+        instance = SaleInvoice.objects.get(id=invoice_id, location=location)
+        days_difference = (timezone.now() - instance.created_at).days
+        return days_difference <= 30
+    except SaleInvoice.DoesNotExist:
+        return False
+
+def check_permission(user_id, location):
+    return AllowRefundPermissionsEmployees.objects.filter(
+        Q(employee_id=user_id) &
+        (
+            Q(allowed_refund__number_of_days__gte=30) |
+            Q(can_refund=True)
+        ) &
+        Q(allowed_refund_location_id=location)
+    ).exists()
