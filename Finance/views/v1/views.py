4b89b425d20c89cac39d6c24a4d1344081fa7f36
M Asad <mughalasad720@gmail.com>
2024-01-09 14:00:37 +0500

    Changes

--- Finance/views/v1/views.py
+++ Finance/views/v1/views.py
@@ -376,4 +376,3 @@ class AllowRefundsAndPermissionsView(APIView):
         except Exception as e:
             return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         
-
