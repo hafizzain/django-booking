e4152e31342b5115c7276eed23487ce119dcb481
hassan <hassanbutt0030@gmail.com>
2024-01-11 03:05:24 +0500

    Employee daily sceduale

--- Employee/serializers.py
+++ Employee/serializers.py
@@ -1,4 +1,5 @@
 from dataclasses import fields
+from django.db.models import Case, When, Value, BooleanField
 from django.db.models import Q
 from genericpath import exists
 from pyexpat import model
@@ -1150,6 +1151,13 @@ class WorkingScheduleSerializer(serializers.ModelSerializer):
         #                                              **query)
         #     # if not qs.exists():
         qs = EmployeDailySchedule.objects.filter(Q(employee=obj) & (Q(is_weekend=True) | Q(is_weekend=False)), **query)
+        qs = qs.annotate(
+            is_vacation_accepted=Case(
+                When(is_vacation=True, vacation_status='accepted', then=Value(True)),
+                default=Value(False),
+                output_field=BooleanField()
+            )
+        ).filter(is_vacation_accepted=True)
 
         is_vacation_qs = qs.filter(is_vacation=True)
 
