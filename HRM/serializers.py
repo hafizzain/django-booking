dff9603206aa1175d2befafcc721d5f1b8141288
MArbabsabir336 <Arbabsabir336@gmail.com>
2024-01-10 12:43:31 +0500

    try to optimize code

--- HRM/serializers.py
+++ HRM/serializers.py
@@ -11,11 +11,14 @@ class HolidaySerializer(serializers.ModelSerializer):
         employee_schedule = None
         start_date = validated_data['start_date']
         end_date = validated_data['end_date']
+        
+        #get all employees on specific location
         all_employees = Employee.objects \
                         .select_related('user', 'business','country', 'state', 'city') \
                         .prefetch_related('location') \
                         .filter(location=validated_data['location'])
-                                        
+        
+        #create holiday schedule for all employees                            
         for employee in all_employees: 
             employee_schedule = EmployeDailySchedule.objects \
                         .create(is_holiday=True,
@@ -23,7 +26,8 @@ class HolidaySerializer(serializers.ModelSerializer):
                                 end_time=end_date,
                                 employee_id = employee.id,
                                 date=end_date)
-        validated_data['employee_schedule_id'] = employee_schedule.id
+                        
+        validated_data['employee_schedule'] = employee_schedule.id
         holiday = Holiday.objects.create(**validated_data)
         return holiday
         
