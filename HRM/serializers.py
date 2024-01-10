2b825d4c58568376a2363f08888a7cab1b00d3ea
MArbabsabir336 <Arbabsabir336@gmail.com>
2024-01-10 20:11:47 +0500

    remove extra code

--- HRM/serializers.py
+++ HRM/serializers.py
@@ -24,15 +24,15 @@ class HolidaySerializer(serializers.ModelSerializer):
 
         # Check if there is any holiday overlapping with the date range
         if start_date is not None and end_date is not None:
-            holiday_check = Holiday.objects.filter(start_date__lte=end_date, end_date__gte=start_date, location=location).exists()
+            holiday_check = Holiday.objects.filter(start_date__lte=end_date,
+                                                    end_date__gte=start_date,
+                                                    location=location).exists()
             if holiday_check:
                 raise serializers.ValidationError({'message': "Holiday already set for this date range."})
 
         return attrs
     
     def create(self, validated_data):
-        employee_schedule_ids = []
-        employee_schedule_id = None
         start_date = validated_data['start_date']
         end_date = validated_data['end_date']
         location = validated_data['location']
