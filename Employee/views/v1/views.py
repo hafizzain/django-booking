036635ebd1c1fdf0a7a76e93885d1f03cf725fa2
hassan <hassanbutt0030@gmail.com>
2024-01-11 11:31:13 +0500

    serializer and views updated

--- Employee/views/v1/views.py
+++ Employee/views/v1/views.py
@@ -4696,6 +4696,7 @@ def create_workingschedule(request):
         working_schedule.note = note
         working_schedule.is_leo_day = True
         working_schedule.is_vacation = False
+        working_schedule.is_weekend = False
         working_schedule.vacation_status = None
         working_schedule.save()
         try:
