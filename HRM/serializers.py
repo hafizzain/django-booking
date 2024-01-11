f64cf7fd2ed81fbbc39d3c1ce022626dda25ee15
hassan <hassanbutt0030@gmail.com>
2024-01-12 00:18:40 +0500

    serializer and views updated

--- HRM/serializers.py
+++ HRM/serializers.py
@@ -10,26 +10,26 @@ class HolidaySerializer(serializers.ModelSerializer):
         model = Holiday
         fields = '__all__'
 
-    def validate(self, attrs):
-        start_date = attrs.get('start_date', None)
-        end_date = attrs.get('end_date', None)
-        location = attrs.get('location', None)
-
-        if start_date is not None:
-            # Check if a holiday already exists for the start_date
-            start_date_check = Holiday.objects.filter(start_date=start_date, location=location).exists()
-            if start_date_check:
-                raise serializers.ValidationError({'message': "Holiday already set for this date."})
-
-        # Check if there is any holiday overlapping with the date range
-        if start_date is not None and end_date is not None:
-            holiday_check = Holiday.objects.filter(start_date__lte=end_date,
-                                                   end_date__gte=start_date,
-                                                   location=location).exists()
-            if holiday_check:
-                raise serializers.ValidationError({'message': "Holiday already set for this date range."})
-
-        return attrs
+    # def validate(self, attrs):
+    #     start_date = attrs.get('start_date', None)
+    #     end_date = attrs.get('end_date', None)
+    #     location = attrs.get('location', None)
+    #
+    #     if start_date is not None:
+    #         # Check if a holiday already exists for the start_date
+    #         start_date_check = Holiday.objects.filter(start_date=start_date, location=location).exists()
+    #         if start_date_check:
+    #             raise serializers.ValidationError({'message': "Holiday already set for this date."})
+    #
+    #     # Check if there is any holiday overlapping with the date range
+    #     if start_date is not None and end_date is not None:
+    #         holiday_check = Holiday.objects.filter(start_date__lte=end_date,
+    #                                                end_date__gte=start_date,
+    #                                                location=location).exists()
+    #         if holiday_check:
+    #             raise serializers.ValidationError({'message': "Holiday already set for this date range."})
+    #
+    #     return attrs
 
     def create(self, validated_data):
         start_date = validated_data['start_date']
