5517cb9a9ed8ea426f26ed063ed656b9959956c6
hassan <hassanbutt0030@gmail.com>
2024-01-11 23:26:59 +0500

    serializer and views updated

--- Employee/serializers.py
+++ Employee/serializers.py
@@ -1201,9 +1201,10 @@ class WorkingScheduleSerializer(serializers.ModelSerializer):
         #     qs = EmployeDailySchedule.objects.filter(Q(employee=obj) & (Q(is_weekend=True) | Q(is_weekend=False)),
         #                                              **query)
         #     # if not qs.exists():
-        qs = EmployeDailySchedule.objects.filter(Q(employee=obj) & (Q(is_weekend=True) | Q(is_weekend=False)), **query)
-        qs = qs.filter((Q(is_vacation=True) & Q(vacation_status='accepted')) | Q(is_leo_day=True) | Q(
+        # qs = EmployeDailySchedule.objects.filter(Q(employee=obj) & (Q(is_weekend=True) | Q(is_weekend=False)), **query)
+        qs = EmployeDailySchedule.filter((Q(is_vacation=True) & Q(vacation_status='accepted')) | Q(is_leo_day=True) | Q(
             is_working_schedule=True) | Q(is_weekend=True) | Q(is_weekend=False))
+        qs = qs.filter(employee=obj)
         # qs = qs.annotate(
         #     is_vacation_accepted=Case(
         #         When(is_vacation=True, vacation_status='accepted', then=Value(True)),
