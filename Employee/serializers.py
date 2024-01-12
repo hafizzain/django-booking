44e14a372bd31a7dce3a46333adb6820d3109f4d
hassan <hassanbutt0030@gmail.com>
2024-01-12 08:26:37 +0500

    serializer file update

--- Employee/serializers.py
+++ Employee/serializers.py
@@ -1027,9 +1027,9 @@ class ScheduleSerializerOP(serializers.ModelSerializer):
             #     return True  # Return True if there is any holiday
             # else:
             #     return Falsedate__date__gte
-            # from HRM.serializers import HolidaySerializer
+            # from HRM.serializers import HolidaySerializer end_date__date__lte=end_date,
             holidays = Holiday.objects.filter(
-                Q(end_date__date__lte=end_date, start_date__date__gte=start_date)
+                Q(start_date__date__gte=start_date)
             )
             if holidays:
                 return True
