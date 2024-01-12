d318fbc7999709e3835be7a265bac77b5b747882
hassan <hassanbutt0030@gmail.com>
2024-01-12 18:21:06 +0500

    serializer file update

--- HRM/serializers.py
+++ HRM/serializers.py
@@ -207,16 +207,16 @@ class HolidaySerializer(serializers.ModelSerializer):
                 to_date = from_date
                 diff = to_date - from_date
                 days = int(diff.days)
-                # for i in range(days + 1):
-                #     if i == 0:
-                #         current_date = from_date
-                #     else:
-                #         current_date = from_date + timedelta(days=i)
-                #     for emp in all_employees:
-                #         working_sch = EmployeDailySchedule.objects.filter(employee_id=emp.id,
-                #                                                           from_date__gte=holiday_start_date,
-                #                                                           end_date=None,
-                #                                                           is_holiday=True)
+                for i in range(days + 1):
+                    if i == 0:
+                        current_date = from_date
+                    else:
+                        current_date = from_date + timedelta(days=i)
+                    # for emp in all_employees:
+                    #     working_sch = EmployeDailySchedule.objects.filter(employee_id=emp.id,
+                    #                                                       from_date__gte=holiday_start_date,
+                    #                                                       end_date=None,
+                    #                                                       is_holiday=True)
                 #         if working_sch:
                 #             working_sch.delete()
                         # EmployeDailySchedule.objects.create(
