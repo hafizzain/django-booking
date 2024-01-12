7c34162148387527ea34de485330016b6743a3af
hassan <hassanbutt0030@gmail.com>
2024-01-12 15:49:38 +0500

    serializer file update

--- HRM/serializers.py
+++ HRM/serializers.py
@@ -212,15 +212,16 @@ class HolidaySerializer(serializers.ModelSerializer):
                     for emp in all_employees:
                         working_sch = EmployeDailySchedule.objects.filter(employee_id=emp.id, date=current_date).first()
                         if working_sch:
-                            working_sch.is_vacation = False
-                            working_sch.is_weekend = False
-                            working_sch.is_holiday = False
-                            working_sch.date = current_date
-                            working_sch.from_date = current_date
+                            break
+                            # working_sch.is_vacation = False
                             # working_sch.is_weekend = False
-                            working_sch.is_vacation = False
-                            working_sch.is_working_schedule = False
-                            working_sch.save()
+                            # working_sch.is_holiday = False
+                            # working_sch.date = current_date
+                            # working_sch.from_date = current_date
+                            # # working_sch.is_weekend = False
+                            # working_sch.is_vacation = False
+                            # working_sch.is_working_schedule = False
+                            # working_sch.save()
                         else:
                             EmployeDailySchedule.objects.create(
                                 employee_id=emp.id,
