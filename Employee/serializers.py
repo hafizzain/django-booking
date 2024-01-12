f91124bf55372bcd0f1d6b7a0cf8dc068d3c2584
hassan <hassanbutt0030@gmail.com>
2024-01-12 10:41:50 +0500

    serializer file update

--- Employee/serializers.py
+++ Employee/serializers.py
@@ -1082,17 +1082,7 @@ class ScheduleSerializerOP(serializers.ModelSerializer):
                 end_date__date__lte=end_date  # Corrected the field name
             )
             if holidays.exists():
-                holidays = holidays.first()
-                if holidays.end_date is None:
-                    holidays_check = Holiday.objects.filter(
-                        start_date__date__gte=start_date,
-                        end_date__date__lte=start_date  # Corrected the field name
-                    )
-                    if holidays_check:
-                        return True
-                    else:
-                        return False
-                # return len(holidays) > 0
+                return len(holidays) > 0
                 # return True
             else:
                 return False
