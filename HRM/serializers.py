b4a8986cfa920e8ac2fc795b384f99a36a23b0d7
hassan <hassanbutt0030@gmail.com>
2024-01-12 17:54:43 +0500

    serializer file update

--- HRM/serializers.py
+++ HRM/serializers.py
@@ -32,6 +32,7 @@ class HolidaySerializer(serializers.ModelSerializer):
         return attrs
 
     def create(self, validated_data):
+
         start_date = validated_data.get('start_date',None)
         end_date = validated_data.get('end_date',None)
         location = validated_data['location']
@@ -192,8 +193,8 @@ class HolidaySerializer(serializers.ModelSerializer):
     def update(self, instance,validated_data):
         holiday_start_date = instance.start_date
         holiday_end_date = instance.end_date
-        start_date = validated_data['start_date']
-        end_date = validated_data['end_date']
+        start_date = validated_data.get('start_date',None)
+        end_date = validated_data.get('end_date',None)
         location = validated_data['location']
         all_employees = Employee.objects \
             .select_related('user', 'business', 'country', 'state', 'city') \
