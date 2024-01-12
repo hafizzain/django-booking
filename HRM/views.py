04c4b154ba05d5f00d14ca9c44b85c932fe13414
MArbabsabir336 <Arbabsabir336@gmail.com>
2024-01-12 15:01:42 +0500

    remove extra query

--- HRM/views.py
+++ HRM/views.py
@@ -47,9 +47,9 @@ class HolidayApiView(APIView):
                 }
             return Response(data, status=status.HTTP_200_OK)
         else:
-            # query = Q()
-            # if location:
-            query &= Q(location=location)
+            query = Q()
+            if location:
+                query &= Q(location=location)
                 
             if name:
                 query &= Q(name__icontains=name)
