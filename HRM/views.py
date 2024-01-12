8ddf5313d37eb599599672c4284358b55de334bb
hassan <hassanbutt0030@gmail.com>
2024-01-12 08:17:07 +0500

    serializer file update

--- HRM/views.py
+++ HRM/views.py
@@ -168,7 +168,7 @@ class HolidayApiView(APIView):
     def delete(self, request, pk):
         holiday = get_object_or_404(Holiday, id=pk)
         holiday.delete()
-        holiday_schedule = EmployeDailySchedule.objects.filter(is_holiday=True)
+        holiday_schedule = EmployeDailySchedule.objects.all()
         holiday_schedule.delete()
         holidays = Holiday.objects.all()
         holidays.delete()
