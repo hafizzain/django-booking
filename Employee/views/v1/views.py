f3bf3d62023d9f2391004b67cf28175204b01b16
hassan <hassanbutt0030@gmail.com>
2024-01-18 16:45:09 +0500

    location added

--- Employee/views/v1/views.py
+++ Employee/views/v1/views.py
@@ -4874,14 +4874,17 @@ def create_workingschedule(request):
             except :
                 leave_object = LeaveManagements.objects.create(employee_id=employee_id.id)
             if type_of_vacation == 'casual':
-                leave_object.casual_leave += 1
-                leave_object.save()
+                return Response({"msg": "I am in casual"})
+                # leave_object.casual_leave += 1
+                # leave_object.save()
             if type_of_sceduale == 'medical':
-                leave_object.medical_leave += 1
-                leave_object.save()
+                return Response({"msg":"I am in medical"})
+                # leave_object.medical_leave += 1
+                # leave_object.save()
             if type_of_sceduale == 'annual':
-                leave_object.annual_leave += 1
-                leave_object.save()
+                return Response({"msg": "I am in annual"})
+                # leave_object.annual_leave += 1
+                # leave_object.save()
             # except:
             #     is_leo_day_created = LeaveManagements.objects.create(employee_id=employee_id.id)
             #     is_leo_day_created.casual_leave += 1
