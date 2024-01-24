228b7d21cceed81e719cc9205c003d7b258aae69
hassan <hassanbutt0030@gmail.com>
2024-01-24 15:25:53 +0500

    url added

--- Utility/Campaign.py
+++ Utility/Campaign.py
@@ -80,17 +80,15 @@ def send_reversal_email(client_phone=None,email=None, appointment_id=None, servi
         name = client_name
         s_name = service_name
         url=url
-        
         # context =
         html_file = render_to_string("AppointmentEmail/reversal.html",
                                      {'url':url,'client':name,'mobile':phone,'email':client_email,'appointment_date':date,'service':s_name,'reason':desc})
         text_content = strip_tags(html_file)
-        # def send_email():
         email = EmailMultiAlternatives(
             'Reversal',
             text_content,
             settings.EMAIL_HOST_USER,
-            to=[email],
+            to=[client_email],
         )
         email.attach_alternative(html_file, "text/html")
         email.send()
