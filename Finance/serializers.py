7dfececbc38bf2ad2f5caa49ac65f1d4b1cf440f
MArbabsabir336 <Arbabsabir336@gmail.com>
2024-01-25 16:30:02 +0500

    debug issue

--- Finance/serializers.py
+++ Finance/serializers.py
@@ -31,7 +31,7 @@ class CouponSerializer(serializers.ModelSerializer):
 class RefundSerializer(serializers.ModelSerializer):
     refunded_products = RefundProductSerializer(many=True)
     refunded_services = RefundServiceSerializer(many=True)
-    related_refund_coupon = CouponSerializer(many=True)
+    related_refund_coupon = CouponSerializer(many=True, read_only=True)
 
     class Meta:
         model = Refund
