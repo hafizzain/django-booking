13366b5f78afc4756d8395a6b7faf20a2c67cc97
hassan <hassanbutt0030@gmail.com>
2024-01-30 18:32:37 +0500

    views coment

--- Appointment/serializers.py
+++ Appointment/serializers.py
@@ -633,7 +633,7 @@ class EmployeeAppointmentSerializer(serializers.ModelSerializer):
                 for id in data['ids']:
                     app_service = AppointmentService.objects.get(id=id)
                     serialized_service = BlockSerializer(app_service,many=False)
-                    serialized_service = AppointmentServiceSerializer(app_service , many=False)
+                    # serialized_service = AppointmentServiceSerializer(app_service , many=False)
                     loop_return.append(serialized_service.data)
                 returned_list.append(loop_return)
 
