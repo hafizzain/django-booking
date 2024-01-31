455fb0a0a268639f1fda0c43c0b0f85e7301eb34
M Asad <mughalasad720@gmail.com>
2024-01-31 15:28:58 +0500

    Changes

--- SaleRecords/views/v1/views.py
+++ SaleRecords/views/v1/views.py
@@ -111,4 +111,4 @@ class SaleRecordViews(APIView):
                 return Response(response_data, status=status.HTTP_201_CREATED)
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         except Exception as e:
-            return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            return Response({"error": str(e), 'First Try':'Error in first Try'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
