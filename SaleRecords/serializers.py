090fe296037bd78566a653da8a7f63f5b01c31ae
M Asad <mughalasad720@gmail.com>
2024-02-01 12:32:15 +0500

    debugging expiry

--- SaleRecords/serializers.py
+++ SaleRecords/serializers.py
@@ -180,12 +180,12 @@ class SaleRecordSerializer(serializers.ModelSerializer):
         applied_gift_cards_records = validated_data.pop('applied_gift_cards_records', [])
         applied_promotions_records = validated_data.pop('applied_promotions_records',[])
         
-        if not any('gift_card' in record for record in gift_cards_records):
-            raise ValueError('gift card is not present not present')
+        # if not any('valid_till' in record for record in gift_cards_records):
+        #     raise ValueError('gift card is not present not present')
         for d in gift_cards_records:
-            expiry = calculate_validity(d['valid'])
+            expiry = calculate_validity(d['valid_till'])
             raise ValueError(str(expiry))
-        raise ValueError('Present')
+        raise ValueError('Not Present')
         
         
         
