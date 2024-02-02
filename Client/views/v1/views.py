dc514e866a9fbb87d99cd739545c678d0a985955
M Asad <mughalasad720@gmail.com>
2024-02-02 17:34:38 +0500

    Changes has been made

--- Client/views/v1/views.py
+++ Client/views/v1/views.py
@@ -2886,10 +2886,7 @@ def get_client_all_gift_cards(request):
     client = request.GET.get('client_id', None)
     code = request.GET.get('code', None)
     
-    query = Q(sale_record__location = location,
-            price__gte=0,
-            expiry__lte=datetime.now(),
-            gift_card__is_custom_card=False)
+    query = Q(sale_record__location = location, price__gt=0, expiry__lte=datetime.now())
     
     if client is not None:
         query &= Q(sale_record__client = client)
