1706c9bdd3f9521214fa17f4014d2b8434f450ce
Huzaifa <huzaifa@192.168.10.5>
2024-02-02 17:12:38 +0500

    create deals api

--- Deal/admin.py
+++ Deal/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
+from .models import Deal, DealCategory
 # Register your models here.
+
+
+@admin.register(Deal)
+class DealAdmin(admin.ModelAdmin):
+    list_display = [
+        'id'
+    ]
+
+
+@admin.register(DealCategory)
+class DealCategoryAdmin(admin.ModelAdmin):
+    list_display = [
+        'id'
+    ]
