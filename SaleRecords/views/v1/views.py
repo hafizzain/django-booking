77a670eef319aff9c820d847362ab9aff23d651c
M Asad <mughalasad720@gmail.com>
2024-02-05 17:15:41 +0500

    Changes

--- SaleRecords/views/v1/views.py
+++ SaleRecords/views/v1/views.py
@@ -1,5 +1,5 @@
-from SaleRecords.models import SaleRecords
-# from Invoices.models import SaleInvoice
+
+from Invoices.models import SaleInvoice
 
 from rest_framework import status
 from rest_framework.response import Response
@@ -75,35 +75,35 @@ class SaleRecordViews(APIView):
             serializer = SaleRecordSerializer(data=request.data, context = {'request': request})
             if serializer.is_valid():
                 sale_record = serializer.save()
-                # try :
-                #     invoice = SaleInvoice.objects.create(
-                #         user=user,
-                #         client=sale_record.client,
-                #         location=sale_record.location,
-                #         appointment=Appointment.objects.get(id = request.data.get('appointment', None)),
-                #         appointment_service=request.data.get('appointment_service', None),
-                #         payment_type=request.data.get('payment_type', None),
-                #         # payment_methods=request.data.get('payment_methods_records'),
+                try :
+                    # invoice = SaleInvoice.objects.create(
+                    #     user=user,
+                    #     client=sale_record.client,
+                    #     location=sale_record.location,
+                    #     appointment=Appointment.objects.get(id = request.data.get('appointment', None)),
+                    #     appointment_service=request.data.get('appointment_service', None),
+                    #     payment_type=request.data.get('payment_type', None),
+                    #     # payment_methods=request.data.get('payment_methods_records'),
 
-                #         # tip=f'{request.data.get("tip")}' if request.data.get("tip") else 0,
-                #         invoice_type='order',
-                #         checkout_type=f'{request.data.get("checkout_type")}' if request.data.get("checkout_type") else 'sale',
+                    #     # tip=f'{request.data.get("tip")}' if request.data.get("tip") else 0,
+                    #     invoice_type='order',
+                    #     checkout_type=f'{request.data.get("checkout_type")}' if request.data.get("checkout_type") else 'sale',
 
-                #         service=f'{request.data.get("service")}' if request.data.get("service") else '',
-                #         member=f'{request.data.get("member")}' if request.data.get("member") else '',
-                #         # business_address=f'{request.data.get("location")}' if request.data.get("location") else '',
-                #         gst=f'{request.data.get("gst")}' if request.data.get("gst") else 0,
-                #         gst_price=f'{request.data.get("gst_price")}' if request.data.get("gst_price") else 0,
-                #         service_price=f'{request.data.get("service_price")}' if request.data.get("service_price") else 0,
-                #         total_price=f'{request.data.get("total_price")}' if request.data.get("total_price") else 0,
-                #         service_commission=f'{request.data.get("service_commission")}' if request.data.get("service_commission") else 0,
-                #         service_commission_type=f'{request.data.get("service_commission_type")}' if request.data.get("service_commission_type") else '',
-                #         checkout=sale_record.id,
-                #     )
-                #     invoice = invoice.save()
-                #     loyalty_points_update(location = location_id , client = client , loyalty_points= sale_record.applied_loyalty_points_records, sub_total=sub_total, invoice = invoice)
-                # except Exception as e:
-                #     return Response({'error':str(e), 'second': 'Second Try'})
+                    #     service=f'{request.data.get("service")}' if request.data.get("service") else '',
+                    #     member=f'{request.data.get("member")}' if request.data.get("member") else '',
+                    #     # business_address=f'{request.data.get("location")}' if request.data.get("location") else '',
+                    #     gst=f'{request.data.get("gst")}' if request.data.get("gst") else 0,
+                    #     gst_price=f'{request.data.get("gst_price")}' if request.data.get("gst_price") else 0,
+                    #     service_price=f'{request.data.get("service_price")}' if request.data.get("service_price") else 0,
+                    #     total_price=f'{request.data.get("total_price")}' if request.data.get("total_price") else 0,
+                    #     service_commission=f'{request.data.get("service_commission")}' if request.data.get("service_commission") else 0,
+                    #     service_commission_type=f'{request.data.get("service_commission_type")}' if request.data.get("service_commission_type") else '',
+                    #     checkout=sale_record.id,
+                    # )
+                    # invoice = invoice.save()
+                    loyalty_points_update(location = location_id , client = client , loyalty_points= sale_record.applied_loyalty_points_records, sub_total=sub_total, invoice = '99477746-3d22-4705-ae1e-99bebfafa5a4')
+                except Exception as e:
+                    return Response({'error':str(e), 'second': 'Second Try'})
                 
                 
                 response_data = {
