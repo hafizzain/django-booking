1e734dbcd02a39ce7cbda9c674b49645777c3142
M Asad <mughalasad720@gmail.com>
2024-02-06 21:39:19 +0500

    Registered the models on admin

--- MultiLanguage/admin.py
+++ MultiLanguage/admin.py
@@ -6,3 +6,15 @@ admin.site.register(Section)
 admin.site.register(Labels)
 admin.site.register(InvoiceTranslation)
 admin.site.register(TranslationLabels)
+
+@admin.register(Language)
+class LanguageAdmin(admin.ModelAdmin):
+    list_display = ['id']
+    
+@admin.register(Section)
+class LanguageAdmin(admin.ModelAdmin):
+    list_display = ['id']
+    
+@admin.register(InvoiceTranslation)
+class LanguageAdmin(admin.ModelAdmin):
+    list_display = ['id','taxes','payment_method']
