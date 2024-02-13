b24255d62a7e7a565bd9d7db27443acc95b01855
MArbabsabir336 <Arbabsabir336@gmail.com>
2024-02-13 17:27:46 +0500

    change serializer name

--- Appointment/views/v1/views.py
+++ Appointment/views/v1/views.py
@@ -4986,7 +4986,7 @@ def update_group_appointment_check_in(request):
                 'status': True,
                 'status_code': 200,
                 'response': {
-                    'message': 'Appointment Checked In Successfuly',
+                    'message': 'Group Checked In Successfuly',
                     'error_message': None,
                     'Appointment' : group_appointment_data
                 }
@@ -4997,7 +4997,7 @@ def update_group_appointment_check_in(request):
                 'status': True,
                 'status_code': 200,
                 'response': {
-                    'message': 'Appointment Checked In Failed',
+                    'message': 'Group Checked In Failed',
                     'error_message': 'Missing Group Appointment ID',
                 }
             }
