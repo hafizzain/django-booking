a503cfa4a4181e10fc5412508c2a79e0a57a7305
M Asad <mughalasad720@gmail.com>
2024-02-15 11:32:34 +0500

    Debugging the client loyalty

--- SaleRecords/views/v1/views.py
+++ SaleRecords/views/v1/views.py
@@ -87,10 +87,10 @@ class SaleRecordViews(APIView):
                     from django.http import JsonResponse
                     from django.core.serializers import serialize
                     
-                    loyalty_points = sale_record.applied_loyalty_points_records
+                    loyalty_points = sale_record.applied_loyalty_points_records.all()
                     # return Response({'loyalty points data': loyalty_points})
                     
-                    serialized_data = serialize('json', loyalty_points)
+                    serialized_data = serialize('json', loyalty_points['fields']['client_loyalty_point'])
 
                     # Return the serialized data as JSON response
                     return JsonResponse(serialized_data, safe=False)
