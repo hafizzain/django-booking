42e4e6438fbc098d8d51712fc159d4ded0f62522
M Asad <mughalasad720@gmail.com>
2024-02-19 13:04:43 +0500

    Added Sale history filteron client name

--- SaleRecords/helpers.py
+++ SaleRecords/helpers.py
@@ -24,6 +24,7 @@ def matching_records(is_quick_sale = None,location=None, range_start=None, range
         
         if search_text is not None:
             filters &= Q(client__full_name__icontains = search_text)
+            
         
         if is_quick_sale:
             matching_records = SaleRecords.objects.filter(filters).order_by('-created_at')[:5]
