9553bf8cd65b0691f7e59d3c7859a091aabbfc5d
M Asad <mughalasad720@gmail.com>
2024-02-20 14:02:29 +0500

    debuggin

--- SaleRecords/serializers.py
+++ SaleRecords/serializers.py
@@ -532,8 +532,10 @@ class SaleRecordSerializer(serializers.ModelSerializer):
                     
                     product_id = item.get("product")
                     # raise ValidationError(f'{str(product_id)}')
-                    product = Product.objects.get(id  = product_id)
-                    
+                    product = Product.objects.get(id  = "99c184b2-3224-4909-9da3-6b2dde8e0e72")
+                    if product:
+                        raise ValidationError(f"product found")
+                    raise ValidationError(f"Not Found")
                     if sale_commission:
                         calculated_commission  = sale_commission.calculated_commission(item.get('price'))
                         EmployeeCommission.objects.create(
