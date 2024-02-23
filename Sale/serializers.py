92133a9e6210a15a69569fe24782a224999c1469
MArbabsabir336 <Arbabsabir336@gmail.com>
2024-02-23 11:31:53 +0500

    try to resolve issue

--- Sale/serializers.py
+++ Sale/serializers.py
@@ -1053,9 +1053,9 @@ class VOSerializerForClientSale(serializers.ModelSerializer):
 
     def get_member(self, obj):
         # return obj.member.full_name
-        if obj.member:
+        try:
             return obj.member.full_name
-        else:
+        except:
             return obj.employee.full_name
 
     def get_voucher(self, obj):
