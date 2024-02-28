7e98ceb4d59d2a940c199cdb01bbae2f4df8afef
M Asad <mughalasad720@gmail.com>
2024-02-28 15:47:21 +0500

    str issue resolved

--- Finance/views/v1/views.py
+++ Finance/views/v1/views.py
@@ -349,7 +349,7 @@ class RefundAPIView(APIView):
                     coupon_data = {
                         'user': request.user.id,
                         'client': client,
-                        'checkout_id': newCheckoutInstance.id,
+                        'checkout_id': str(newCheckoutInstance.id),
                         'refund_coupon_code': f"{short_uuid(refund_instance.id)}",
                         'amount': refund_instance.total_refund_amount,
                         'expiry_date': expiry_date,
