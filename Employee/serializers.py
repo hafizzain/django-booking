7d01b48adb98ea4de840c3d52afadf5c648a5710
Muhammad Arbab Sabir <Arbabsabir336@gmail.com>
2024-03-05 11:50:18 +0500

    debugin data issue

--- Employee/serializers.py
+++ Employee/serializers.py
@@ -1138,7 +1138,7 @@ class ScheduleSerializerOP(serializers.ModelSerializer):
             employee_obj = self.context.get('obj')
             location = self.context.get('location_id', None)
             daily_schedule = EmployeDailySchedule.objects.filter(employee=employee_obj.id).values_list('date', flat=True)
-            break_time = BrakeTime.objects.filter(employee=employee_obj.id, date=daily_schedule.date, location=location)
+            break_time = BrakeTime.objects.filter(employee=employee_obj.id, date__in=daily_schedule, location=location)
             return BrakeTimeSerializer(break_time, many=True).data
         except Exception as err:
             return str(err)
