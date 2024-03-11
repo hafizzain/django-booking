059b1229d246865a2487abd2d09d5f0f42c8a9e8
M Asad <mughalasad720@gmail.com>
2024-03-11 11:18:21 +0500

    Changes

--- SaleRecords/signals.py
+++ SaleRecords/signals.py
@@ -1,5 +1,6 @@
 from django.dispatch import receiver
 from django.db.models.signals import post_save
+from rest_framework.response import Response
 from .models import *
 
 
@@ -15,20 +16,23 @@ from .models import *
 
 @receiver(post_save, sender=MembershipInstallments)
 def next_installment_expiry(sender, instance, created, **kwargs):
-    if created:  # Check if the instance is newly created
-        membership = instance.membership  # Assuming membership is the ForeignKey field in MembershipInstallments
-        if membership.installment_months:
-            # Assuming calculate_validity function is defined elsewhere and correctly calculates the next installment date
-            next_membership_expiry = relativedelta(membership.created_at, instance.created_at).months + 1 
-            membership.next_installment_date = calculate_validity(str(next_membership_expiry)+ ' months')
-            total_paid_installments = MembershipInstallments.objects.filter(membership = instance.membership).count()
-            membership.remaining_installments = membership.installment_months - total_paid_installments
-            
-            # currency_id = 
-            memberhsip_instance = CurrencyPriceMembership.objects.filter(
-                                                    currency_id=membership.sale_record.location.currency.id,
-                                                    membership_id=membership.membership.id
-                                                ).first()
-            membership.payable_amount = memberhsip_instance.price - instance.paid_installment
-            
-            membership.save()
+    try:
+        if created:  # Check if the instance is newly created
+            membership = instance.membership  # Assuming membership is the ForeignKey field in MembershipInstallments
+            if membership.installment_months:
+                # Assuming calculate_validity function is defined elsewhere and correctly calculates the next installment date
+                next_membership_expiry = relativedelta(membership.created_at, instance.created_at).months + 1 
+                membership.next_installment_date = calculate_validity(str(next_membership_expiry)+ ' months')
+                total_paid_installments = MembershipInstallments.objects.filter(membership = instance.membership).count()
+                membership.remaining_installments = membership.installment_months - total_paid_installments
+                
+                # currency_id = 
+                memberhsip_instance = CurrencyPriceMembership.objects.filter(
+                                                        currency_id=membership.sale_record.location.currency.id,
+                                                        membership_id=membership.membership.id
+                                                    ).first()
+                membership.payable_amount = memberhsip_instance.price - instance.paid_installment
+                
+                membership.save()
+    except Exception as e:
+        return Response({'error: error occured in signal'})
